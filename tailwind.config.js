d9f275681c8212a338e70cc1de6bbc6217f0ad01
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-20 12:03:13 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}", // Tremor module
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
