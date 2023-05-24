20c2297e8126c6cf2578e7ffe5a52b906e8344de
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-25 00:23:49 +0100

    ~

--- pages/api/auth/github/index.ts
+++ pages/api/auth/github/index.ts
@@ -1,14 +1,50 @@
-import passport from "@/lib/passport-github-auth";
-import nextConnect from "next-connect";
+import { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+import { PrismaClient } from "@prisma/client";
+import querystring from "querystring";
 
-const handler = nextConnect();
+const { GITHUB_CLIENT_ID, GITHUB_SECRET } = process.env;
 
-handler.use(passport.initialize());
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { code } = req.query;
 
-handler.get(
-  passport.authenticate("github", {
-    scope: ["profile", "email"],
-  })
-);
+    // Exchange the authorization code with an access token
+    const { data: tokenResponse } = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        code,
+        client_id: GITHUB_CLIENT_ID,
+        client_secret: GITHUB_SECRET,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
 
-export default handler;
+    const { access_token: accessToken } = tokenResponse;
+
+    const { data: userResponse } = await axios.get(
+      "https://api.github.com/user",
+      {
+        headers: {
+          Authorization: `token ${accessToken}`,
+        },
+      }
+    );
+
+    const { email, login: username, id: githubId } = userResponse;
+
+    res.redirect("/");
+  } catch (error) {
+    console.error("GitHub authentication error:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred during authentication", err: error });
+  }
+}
