7896cb57d7041ca729e08056d28c4081f9345d06
bossoncode <akinkunmiisrael41@gmail.com>
2023-05-25 02:23:09 +0100

    .

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,10 @@
-import { signIn } from "next-auth/react";
 import React from "react";
 
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold text-center mb-8">Login bro</h1>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => signIn()}
-      >
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Loign with GitHub
       </button>
     </div>
