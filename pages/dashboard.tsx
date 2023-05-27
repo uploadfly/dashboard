0ca2cbec1b774505977e06a85f2307ebe64160c0
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-27 21:08:06 +0100

    gtfo

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,21 @@
+import axios from "axios";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <button
+        onClick={() => {
+          axios.post("http://localhost:1112/logout").then(() => {
+            window.location.href = "/";
+            console.log("logged out");
+          });
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
 };
 
 export default Dashboard;
