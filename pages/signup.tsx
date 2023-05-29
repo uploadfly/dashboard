af469dd9ed7414e303ff22fdaa59d4a255486471
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-29 11:25:37 +0100

    /signup

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,8 +1,110 @@
+import { toastErrorConfig, toastSuccessConfig } from "@/configs/toast";
 import AuthLayout from "@/layouts/AuthLayout";
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Signup = () => {
-  return <AuthLayout text="Get started">Signup</AuthLayout>;
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const signupWithEmail = async () => {
+    if (!email) {
+      toast("Email is required", toastErrorConfig);
+      return;
+    }
+
+    if (!password) {
+      toast("Password is required", toastErrorConfig);
+      return;
+    }
+
+    if (!confirmPassword) {
+      toast("Type your password again", toastErrorConfig);
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      toast("Passwords do not match", toastErrorConfig);
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_AUTH_URL}/signup`,
+        {
+          email,
+          password,
+          confirmPassword,
+        }
+      );
+      console.log(res);
+      setLoading(false);
+    } catch (error: any) {
+      console.log(error.response);
+      setLoading(false);
+      toast(error.response?.data?.message, toastErrorConfig);
+    }
+  };
+
+  return (
+    <AuthLayout text="Get started">
+      <form
+        className="flex flex-col gap-8 z-40"
+        onSubmit={(e) => {
+          e.preventDefault();
+          signupWithEmail();
+        }}
+      >
+        <input
+          type="text"
+          className="input"
+          placeholder="What's your email?"
+          value={email}
+          onChange={(e) => setEmail(e.target.value.toLowerCase().trim())}
+        />
+
+        <input
+          type="password"
+          className="input"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <input
+          type="password"
+          className="input"
+          placeholder="Shh...it's a secret"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        <button className="uf-gradient w-full rounded-md py-2 h-10 flex items-center justify-center text-[#1e1e1e] font-bold hover:scale-105 transition-all">
+          {loading ? (
+            <span className="animate-slide w-1/2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-7 text-uf-dark"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+                />
+              </svg>
+            </span>
+          ) : (
+            <span className="shadow-2xl">Continue</span>
+          )}
+        </button>
+      </form>
+    </AuthLayout>
+  );
 };
 
 export default Signup;
