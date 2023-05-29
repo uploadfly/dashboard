79f77048a7e47eca3128cf00bbd203a66fa0ec14
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-29 10:11:01 +0100

    .

---
+++ pages/signup.tsx
@@ -0,0 +1,8 @@
+import AuthLayout from "@/layouts/AuthLayout";
+import React from "react";
+
+const Signup = () => {
+  return <AuthLayout text="Get started">Signup</AuthLayout>;
+};
+
+export default Signup;
