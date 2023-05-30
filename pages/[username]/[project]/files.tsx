7fdf4b48d8cb8cd1d28fa3bf37993bd4bfe4c7c2
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-31 00:21:03 +0100

    initial commit

---
+++ pages/[username]/[project]/files.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+import React from "react";
+
+const Files = () => {
+  return <DashboardLayout>Links</DashboardLayout>;
+};
+
+export default Files;
