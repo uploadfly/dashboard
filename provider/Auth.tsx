1386d3fe98c13eee1fbdd8f05f0060cb6e7999be
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-30 21:36:36 +0100

    initial commit

---
+++ provider/Auth.tsx
@@ -0,0 +1,24 @@
+import { ReactNode, useEffect } from "react";
+import { useRouter } from "next/router";
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const cookies = document.cookie.split(";");
+
+    const expCookie = cookies.find((cookie) =>
+      cookie.trim().startsWith("exp=")
+    );
+    // Written by ChatGPT
+    // Modified by @xing0x
+
+    if (!expCookie) {
+      router.push("/login");
+      return;
+    }
+  }, []);
+  return <>{children}</>;
+};
+
+export default AuthProvider;
