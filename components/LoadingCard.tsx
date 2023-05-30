82aaab1c827da0f172856b71b028a45d879f891f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-30 06:57:48 +0100

    initial commit

---
+++ components/LoadingCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoadingCard = () => {
+  return (
+    <div className="animate-breath uf-gradient rounded-md w-[380px] h-[200px]"></div>
+  );
+};
+
+export default LoadingCard;
