75e812ad61a70ff212394aa4ebf0d5155cc461f0
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-30 23:33:48 +0100

    initial commit

---
+++ pages/[username]/[project]/index.tsx
@@ -0,0 +1,14 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+
+const Project = () => {
+  return (
+    <DashboardLayout>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
+        dolorem!
+      </p>
+    </DashboardLayout>
+  );
+};
+
+export default Project;
