897427f74d450676284664f215ead2b8bf1e0e54
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-31 00:21:39 +0100

    rendered sidebar

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
 import React, { ReactNode } from "react";
 
 const DashboardLayout = ({ children }: { children: ReactNode }) => {
@@ -6,7 +7,9 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
     <div className="bg-uf-dark text-uf-light h-screen overflow-hidden">
       <Navbar />
       <div className="h-screen flex w-full">
-        <div className="w-[20%] p-5 bg-[#050505] sticky top-1">hmmmm</div>
+        <div className="w-[20%] p-5 bg-[#050505] sticky top-1">
+          <Sidebar />
+        </div>
         <div className="w-[80%] p-5 overflow-auto mb-20">{children}</div>
       </div>
     </div>
