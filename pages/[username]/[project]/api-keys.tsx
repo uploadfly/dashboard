7e17e5dede427fb5b58ef567c445c007cc03773e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-31 01:14:15 +0100

    initial commit

---
+++ pages/[username]/[project]/api-keys.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+import React from "react";
+
+const ApiKeys = () => {
+  return <DashboardLayout>ApiKeys</DashboardLayout>;
+};
+
+export default ApiKeys;
