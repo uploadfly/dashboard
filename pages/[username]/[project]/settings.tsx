a1bb37e0f8b291728f651b1c352b3dbf1f22dcce
akinloluwami <akinkunmiisrael41@gmail.com>
2023-05-31 01:14:21 +0100

    //

---
+++ pages/[username]/[project]/settings.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+import React from "react";
+
+const Settings = () => {
+  return <DashboardLayout>Settings</DashboardLayout>;
+};
+
+export default Settings;
