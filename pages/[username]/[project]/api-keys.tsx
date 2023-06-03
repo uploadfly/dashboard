53d9eac8e18d66556cfadbcfa8de250c3b6d7833
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-03 10:58:24 +0100

    fetch keys

--- pages/[username]/[project]/api-keys.tsx
+++ pages/[username]/[project]/api-keys.tsx
@@ -1,7 +1,28 @@
+import { axios } from "@/configs/axios";
 import DashboardLayout from "@/layouts/DashboardLayout";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ApiKeys = () => {
+  const [keys, setKeys] = useState<{ sk: string; pk: string }>({
+    sk: "",
+    pk: "",
+  });
+
+  const fetchKeys = async () => {
+    try {
+      const res = await axios.get("/api-keys");
+      setKeys(res.data);
+    } catch {
+      setKeys({ sk: "", pk: "" });
+    }
+  };
+
+  useEffect(() => {
+    fetchKeys();
+  }, []);
+
+  const regenerateKeys = async () => {};
+
   return (
     <DashboardLayout>
       <h1 className="text-2xl font-semibold">API Keys</h1>
@@ -41,7 +62,21 @@ const ApiKeys = () => {
             complete access to your Uploadfly cloud.
           </p>
         </div>
-        <button>Regenerate</button>
+        <button className="flex items-center gap-4 bg-uf-light text-uf-dark px-3 py-3 rounded-md font-semibold">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Regenerate
+        </button>
       </div>
     </DashboardLayout>
   );
