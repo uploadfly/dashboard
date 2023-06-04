c1dd89c713450a7711f6c0f0eb4005fddc63ba3a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-05 00:16:20 +0100

    .

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -1,8 +1,21 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
-import React from "react";
+import React, { useState } from "react";
 
 const Files = () => {
-  return <DashboardLayout>Links</DashboardLayout>;
+  const [showFileInfo, setShowFileInfo] = useState(false);
+  return (
+    <DashboardLayout>
+      <div className="bg-blue-500 h-screen flex">
+        <div className="h-full bg-red-400 w-[35%]"></div>
+        <div className="h-full bg-purple-500  w-full"></div>
+        <div
+          className={`transition-all duration-500 ease-in-out h-full bg-green-500 ${
+            showFileInfo ? "w-[45%]" : "w-0"
+          }`}
+        ></div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Files;
