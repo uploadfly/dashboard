7e39759030a0ad6be64ab67af727f35d545d7236
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-04 22:00:16 +0100

    initial commit

---
+++ pages/[username]/[project]/rules.tsx
@@ -0,0 +1,8 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+import React from "react";
+
+const Rules = () => {
+  return <DashboardLayout>Rules</DashboardLayout>;
+};
+
+export default Rules;
