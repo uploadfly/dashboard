dc1389c9bdb927e6966ba165b7deede4471da79b
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-06 21:29:17 +0100

    map

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -7,6 +7,7 @@ interface File {
   mimetype: string;
   size: number;
   url: string;
+  uploaded: Date;
 }
 
 const files: File[] = [
@@ -16,6 +17,7 @@ const files: File[] = [
     mimetype: "text/plain",
     size: 1024,
     url: "https://example.com/file1.txt",
+    uploaded: new Date("2023-06-01"),
   },
   {
     id: 2,
@@ -23,6 +25,7 @@ const files: File[] = [
     mimetype: "image/jpeg",
     size: 2048,
     url: "https://example.com/image.jpg",
+    uploaded: new Date("2023-06-02"),
   },
   {
     id: 3,
@@ -30,43 +33,106 @@ const files: File[] = [
     mimetype: "application/pdf",
     size: 4096,
     url: "https://example.com/document.pdf",
+    uploaded: new Date("2023-06-03"),
   },
   {
     id: 4,
-    name: "presentation.pptx",
-    mimetype:
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    name: "video.mp4",
+    mimetype: "video/mp4",
     size: 8192,
-    url: "https://example.com/presentation.pptx",
+    url: "https://example.com/video.mp4",
+    uploaded: new Date("2023-06-04"),
   },
   {
     id: 5,
-    name: "spreadsheet.xlsx",
+    name: "presentation.pptx",
     mimetype:
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
     size: 16384,
-    url: "https://example.com/spreadsheet.xlsx",
+    url: "https://example.com/presentation.pptx",
+    uploaded: new Date("2023-06-05"),
   },
   {
-    id: 18,
+    id: 6,
     name: "audio.mp3",
     mimetype: "audio/mpeg",
     size: 32768,
     url: "https://example.com/audio.mp3",
+    uploaded: new Date("2023-06-06"),
   },
   {
-    id: 19,
-    name: "video.mp4",
-    mimetype: "video/mp4",
+    id: 7,
+    name: "spreadsheet.xlsx",
+    mimetype:
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
     size: 65536,
-    url: "https://example.com/video.mp4",
+    url: "https://example.com/spreadsheet.xlsx",
+    uploaded: new Date("2023-06-07"),
   },
   {
-    id: 20,
-    name: "archive.zip",
-    mimetype: "application/zip",
+    id: 8,
+    name: "code.js",
+    mimetype: "application/javascript",
     size: 131072,
-    url: "https://example.com/archive.zip",
+    url: "https://example.com/code.js",
+    uploaded: new Date("2023-06-08"),
+  },
+  {
+    id: 9,
+    name: "image2.png",
+    mimetype: "image/png",
+    size: 262144,
+    url: "https://example.com/image2.png",
+    uploaded: new Date("2023-06-09"),
+  },
+  {
+    id: 10,
+    name: "document2.pdf",
+    mimetype: "application/pdf",
+    size: 524288,
+    url: "https://example.com/document2.pdf",
+    uploaded: new Date("2023-06-10"),
+  },
+  {
+    id: 11,
+    name: "file2.txt",
+    mimetype: "text/plain",
+    size: 1048576,
+    url: "https://example.com/file2.txt",
+    uploaded: new Date("2023-06-11"),
+  },
+  {
+    id: 12,
+    name: "image3.jpg",
+    mimetype: "image/jpeg",
+    size: 2097152,
+    url: "https://example.com/image3.jpg",
+    uploaded: new Date("2023-06-12"),
+  },
+  {
+    id: 13,
+    name: "document3.pdf",
+    mimetype: "application/pdf",
+    size: 4194304,
+    url: "https://example.com/document3.pdf",
+    uploaded: new Date("2023-06-13"),
+  },
+  {
+    id: 14,
+    name: "video2.mp4",
+    mimetype: "video/mp4",
+    size: 8388608,
+    url: "https://example.com/video2.mp4",
+    uploaded: new Date("2023-06-14"),
+  },
+  {
+    id: 15,
+    name: "presentation2.pptx",
+    mimetype:
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    size: 16777216,
+    url: "https://example.com/presentation2.pptx",
+    uploaded: new Date("2023-06-15"),
   },
 ];
 
@@ -100,7 +166,7 @@ const File = ({
   const [showCheckIcon, setShowCheckIcon] = useState(false);
   return (
     <div
-      className="flex items-center cursor-pointer gap-2 justify-between mb-3 px-4 py-2 bg-slate-900 w-full rounded-md hover:scale-105 transition-all duration-300"
+      className="flex items-center cursor-pointer gap-2 justify-between mb-3 px-4 py-2 bg-uf-dark w-full rounded-md hover:scale-105 transition-all duration-300"
       onClick={() => {
         setShowCheckIcon(!showCheckIcon);
         onClick();
@@ -163,19 +229,28 @@ const Files = () => {
           <div className="">
             {files.map((file) => (
               <File
-                name="name"
-                size={100}
-                type="type"
-                onClick={() => setSelectedFile(file)}
+                name={file.name}
+                size={file.size}
+                type={file.mimetype}
+                onClick={() => {
+                  if (selectedFile?.id === file.id) {
+                    setSelectedFile(null);
+                    return;
+                  }
+                  setSelectedFile(file);
+                }}
               />
             ))}
           </div>
         </div>
         <div
           className={`transition-all duration-500 ease-in-out h-full ${
-            showFileInfo ? "w-[45%]" : "w-0"
+            selectedFile !== null ? "w-[45%]" : "w-0"
           }`}
-        ></div>
+        >
+          <p>{selectedFile?.name}</p>
+          <p>{selectedFile?.size}</p>
+        </div>
       </div>
     </DashboardLayout>
   );
