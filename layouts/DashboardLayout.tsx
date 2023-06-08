c2cc7da1a9c5c7f4eccd95801efc77fa99ec7c48
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-08 22:05:45 +0100

    ...

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -33,7 +33,7 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
         <div className="w-[20%] p-5 bg-[#050505] sticky top-1">
           <Sidebar />
         </div>
-        <div className="w-[80%] py-5 px-10 overflow-y-scroll mb-20">
+        <div className="w-[80%] mt-5 px-10 overflow-y-scroll mb-20">
           {/* {status === 404 ? (
             <h1>Not found</h1>
           ) : status === 500 ? (
