15eb4709aa56106b619adaff959bd66165290fc3
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-08 21:08:39 +0100

    many many things

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -1,140 +1,7 @@
+import { files } from "@/files";
+import { File } from "@/interfaces";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import React, { useState } from "react";
-
-interface File {
-  id: number;
-  name: string;
-  mimetype: string;
-  size: number;
-  url: string;
-  uploaded: Date;
-}
-
-const files: File[] = [
-  {
-    id: 1,
-    name: "file1.txt",
-    mimetype: "text/plain",
-    size: 1024,
-    url: "https://example.com/file1.txt",
-    uploaded: new Date("2023-06-01"),
-  },
-  {
-    id: 2,
-    name: "image.jpg",
-    mimetype: "image/jpeg",
-    size: 2048,
-    url: "https://example.com/image.jpg",
-    uploaded: new Date("2023-06-02"),
-  },
-  {
-    id: 3,
-    name: "document.pdf",
-    mimetype: "application/pdf",
-    size: 4096,
-    url: "https://example.com/document.pdf",
-    uploaded: new Date("2023-06-03"),
-  },
-  {
-    id: 4,
-    name: "video.mp4",
-    mimetype: "video/mp4",
-    size: 8192,
-    url: "https://example.com/video.mp4",
-    uploaded: new Date("2023-06-04"),
-  },
-  {
-    id: 5,
-    name: "presentation.pptx",
-    mimetype:
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    size: 16384,
-    url: "https://example.com/presentation.pptx",
-    uploaded: new Date("2023-06-05"),
-  },
-  {
-    id: 6,
-    name: "audio.mp3",
-    mimetype: "audio/mpeg",
-    size: 32768,
-    url: "https://example.com/audio.mp3",
-    uploaded: new Date("2023-06-06"),
-  },
-  {
-    id: 7,
-    name: "spreadsheet.xlsx",
-    mimetype:
-      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-    size: 65536,
-    url: "https://example.com/spreadsheet.xlsx",
-    uploaded: new Date("2023-06-07"),
-  },
-  {
-    id: 8,
-    name: "code.js",
-    mimetype: "application/javascript",
-    size: 131072,
-    url: "https://example.com/code.js",
-    uploaded: new Date("2023-06-08"),
-  },
-  {
-    id: 9,
-    name: "image2.png",
-    mimetype: "image/png",
-    size: 262144,
-    url: "https://example.com/image2.png",
-    uploaded: new Date("2023-06-09"),
-  },
-  {
-    id: 10,
-    name: "document2.pdf",
-    mimetype: "application/pdf",
-    size: 524288,
-    url: "https://example.com/document2.pdf",
-    uploaded: new Date("2023-06-10"),
-  },
-  {
-    id: 11,
-    name: "file2.txt",
-    mimetype: "text/plain",
-    size: 1048576,
-    url: "https://example.com/file2.txt",
-    uploaded: new Date("2023-06-11"),
-  },
-  {
-    id: 12,
-    name: "image3.jpg",
-    mimetype: "image/jpeg",
-    size: 2097152,
-    url: "https://example.com/image3.jpg",
-    uploaded: new Date("2023-06-12"),
-  },
-  {
-    id: 13,
-    name: "document3.pdf",
-    mimetype: "application/pdf",
-    size: 4194304,
-    url: "https://example.com/document3.pdf",
-    uploaded: new Date("2023-06-13"),
-  },
-  {
-    id: 14,
-    name: "video2.mp4",
-    mimetype: "video/mp4",
-    size: 8388608,
-    url: "https://example.com/video2.mp4",
-    uploaded: new Date("2023-06-14"),
-  },
-  {
-    id: 15,
-    name: "presentation2.pptx",
-    mimetype:
-      "application/vnd.openxmlformats-officedocument.presentationml.presentation",
-    size: 16777216,
-    url: "https://example.com/presentation2.pptx",
-    uploaded: new Date("2023-06-15"),
-  },
-];
 
 const Button = ({ text }: { text: string }) => {
   return (
@@ -166,18 +33,13 @@ const File = ({
   const [showCheckIcon, setShowCheckIcon] = useState(false);
   return (
     <div
-      className="flex items-center cursor-pointer gap-2 justify-between mb-3 px-4 py-2 bg-[#050505] w-full rounded-md transition-all duration-300"
+      className="flex items-center cursor-pointer gap-2 justify-between mb-3 px-4 py-2 bg-[#050505] w-[220px] rounded-md transition-all duration-300 relative"
       onClick={() => {
         setShowCheckIcon(!showCheckIcon);
         onClick();
       }}
     >
       <div className="flex items-center">
-        <img
-          src="https://icon-library.com/images/icon-images-for-windows/icon-images-for-windows-8.jpg"
-          alt=""
-          className="w-10 h-10 mr-5"
-        />
         <div className="">
           <p className="text-lg mb-1">{name}</p>
           <div className="flex gap-3 items-center">
@@ -186,7 +48,7 @@ const File = ({
           </div>
         </div>
       </div>
-      <div className="bg-slate-700 w-5 h-5 rounded-md flex items-center justify-center">
+      <div className="bg-slate-700 w-5 h-5 rounded-md flex items-center justify-center absolute right-2 top-2">
         {showCheckIcon && (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -211,7 +73,7 @@ const Files = () => {
   return (
     <DashboardLayout>
       <div className="flex gap-5">
-        <div className="h-full w-[35%] p-1 flex flex-col items-end">
+        <div className="h-full w-[35%] flex flex-col items-end sticky top-0">
           <Button text="Root" />
           <div className="w-[90%] h-fit">
             <Button text="Root" />
@@ -223,9 +85,25 @@ const Files = () => {
             <Button text="Root" />
           </div>
         </div>
-        <div className="h-full w-full mb-10">
-          <div className="w-full h-16"></div>
-          <div className="h-screen py-3 overflow-y-scroll">
+        <div className="h-full w-full">
+          <div className="w-full h-16 sticky top-0 z-10 bg-uf-dark">
+            <div className="">
+              {selectedFile?.length > 1 ? (
+                <>
+                  <p>{selectedFile?.length} files selected</p>
+                  <p>
+                    {selectedFile?.reduce((acc, curr) => acc + curr.size, 0)}kb
+                  </p>
+                </>
+              ) : (
+                <>
+                  <p>{selectedFile?.[0]?.name}</p>
+                  <p>{selectedFile?.[0]?.size}</p>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-2">
             {files.map((file) => (
               <File
                 key={file.id}
@@ -242,23 +120,6 @@ const Files = () => {
               />
             ))}
           </div>
-        </div>
-        <div
-          className={`transition-all duration-500 ease-in-out h-full ${
-            selectedFile.length > 0 ? "w-[45%]" : "w-0"
-          }`}
-        >
-          {selectedFile?.length > 1 ? (
-            <>
-              <p>{selectedFile?.length} files selected</p>
-              <p>{selectedFile?.reduce((acc, curr) => acc + curr.size, 0)}kb</p>
-            </>
-          ) : (
-            <>
-              <p>{selectedFile?.[0]?.name}</p>
-              <p>{selectedFile?.[0]?.size}</p>
-            </>
-          )}
         </div>
       </div>
     </DashboardLayout>
