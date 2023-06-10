0bf5a9f9c5c2d5642abb55c1d6c32c4cc7fd633c
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-10 22:41:15 +0100

    ...

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -1,5 +1,6 @@
+import File from "@/components/File";
 import { files } from "@/files";
-import { File } from "@/interfaces";
+import { FileProps } from "@/interfaces";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import React, { useState } from "react";
 
@@ -19,55 +20,8 @@ const Button = ({ text }: { text: string }) => {
   );
 };
 
-const File = ({
-  name,
-  size,
-  type,
-  selected,
-  onClick,
-}: {
-  name: string;
-  size: number;
-  type: string;
-  selected: boolean;
-  onClick: () => void;
-}) => {
-  return (
-    <div
-      className="flex items-center cursor-pointer gap-2 justify-between mb-3 px-4 py-2 bg-[#050505] w-[235px] rounded-md transition-all duration-300 relative"
-      onClick={onClick}
-    >
-      <div className="flex items-center">
-        <div className="">
-          <p className="text-lg mb-1">{name}</p>
-          <div className="flex gap-3 items-center">
-            <p className="text-xs text-slate-400">Uploaded 2 days ago</p>
-            <p className="text-xs text-slate-400">{size}kb</p>
-          </div>
-        </div>
-      </div>
-      <div className="bg-slate-700 w-5 h-5 rounded-md flex items-center justify-center absolute right-2 top-2">
-        {selected && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-4 h-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clipRule="evenodd"
-            />
-          </svg>
-        )}
-      </div>
-    </div>
-  );
-};
-
 const Files = () => {
-  const [selectedFile, setSelectedFile] = useState<File[]>([]);
+  const [selectedFile, setSelectedFile] = useState<FileProps[]>([]);
 
   const selection = (type?: "all") => {
     if (type === "all") {
@@ -99,9 +53,14 @@ const Files = () => {
                 <p>{files.length} files</p>
                 <p>{files.reduce((acc, curr) => acc + curr.size, 0)} bytes</p>
               </div>
-              <div className="flex items-center">
+              <div className="flex items-center gap-4">
                 <button></button>
-                <button onClick={() => selection("all")}>Select all</button>
+                <button
+                  onClick={() => selection("all")}
+                  className="border border-gray-50 py-2 px-4 font-semibold rounded-md"
+                >
+                  Select all
+                </button>
                 <button className="bg-uf-light text-uf-dark flex items-center py-2 px-4 font-semibold gap-2 rounded-md">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -125,12 +84,26 @@ const Files = () => {
               }`}
             >
               {selectedFile?.length > 1 ? (
-                <>
-                  <p>{selectedFile?.length} files selected</p>
-                  <p>
-                    {selectedFile?.reduce((acc, curr) => acc + curr.size, 0)}kb
-                  </p>
-                </>
+                <div className="flex items-center justify-between w-full">
+                  <div className="">
+                    <p>{selectedFile?.length} files selected</p>
+                    <p>
+                      {selectedFile?.reduce((acc, curr) => acc + curr.size, 0)}
+                      kb
+                    </p>
+                  </div>
+                  <div className="flex gap-4">
+                    <button
+                      onClick={() => selection()}
+                      className="border border-gray-50 py-2 px-4 font-semibold rounded-md"
+                    >
+                      Deselect all
+                    </button>
+                    <button className="py-2 px-4 bg-red-600 font-semibold rounded-md text-uf-light">
+                      Delete all
+                    </button>
+                  </div>
+                </div>
               ) : (
                 <>
                   <p>{selectedFile?.[0]?.name}</p>
