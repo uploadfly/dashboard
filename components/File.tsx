56b72b8968af69f19a0e06b8bdc15147a8c0b6d4
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-10 22:57:12 +0100

    svg to svg

--- components/File.tsx
+++ components/File.tsx
@@ -1,3 +1,5 @@
+import { BsCheckLg } from "react-icons/bs";
+
 const File = ({
   name,
   size,
@@ -26,20 +28,7 @@ const File = ({
         </div>
       </div>
       <div className="bg-slate-700 w-5 h-5 rounded-md flex items-center justify-center absolute right-2 top-2">
-        {selected && (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className="w-4 h-4"
-          >
-            <path
-              fillRule="evenodd"
-              d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-              clipRule="evenodd"
-            />
-          </svg>
-        )}
+        {selected && <BsCheckLg />}
       </div>
     </div>
   );
