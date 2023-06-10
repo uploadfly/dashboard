1ebfaa7a3f84dcc905eac49c8371a04c49db5464
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-10 23:14:07 +0100

    icon

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -4,18 +4,12 @@ import { FileProps } from "@/interfaces";
 import DashboardLayout from "@/layouts/DashboardLayout";
 import { useState } from "react";
 import { CgSoftwareUpload } from "react-icons/cg";
+import { HiFolderOpen } from "react-icons/hi2";
 
 const Button = ({ text }: { text: string }) => {
   return (
     <button className="flex items-center gap-2 mb-3 px-4 py-2 bg-[#050505] w-full rounded-md">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="currentColor"
-        className="w-5 h-5"
-      >
-        <path d="M3.75 3A1.75 1.75 0 002 4.75v3.26a3.235 3.235 0 011.75-.51h12.5c.644 0 1.245.188 1.75.51V6.75A1.75 1.75 0 0016.25 5h-4.836a.25.25 0 01-.177-.073L9.823 3.513A1.75 1.75 0 008.586 3H3.75zM3.75 9A1.75 1.75 0 002 10.75v4.5c0 .966.784 1.75 1.75 1.75h12.5A1.75 1.75 0 0018 15.25v-4.5A1.75 1.75 0 0016.25 9H3.75z" />
-      </svg>
+      <HiFolderOpen />
       {text}
     </button>
   );
