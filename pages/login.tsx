c871ce592162fd8e437dfd735b3f4eb1b8b830fb
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-17 20:42:13 +0100

    added loading spinner

--- pages/login.tsx
+++ pages/login.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 import { axiosAuth } from "@/configs/axios";
+import { RiLoader5Fill } from "react-icons/ri";
 
 const Login = () => {
   const [email, setEmail] = useState<string>("");
@@ -67,21 +68,8 @@ const Login = () => {
           disabled={loading}
         >
           {loading ? (
-            <span className="animate-slide w-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-7 h-7 text-uf-dark"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                />
-              </svg>
+            <span className="animate-spin text-2xl">
+              <RiLoader5Fill />
             </span>
           ) : (
             <span className="shadow-2xl">Login</span>
