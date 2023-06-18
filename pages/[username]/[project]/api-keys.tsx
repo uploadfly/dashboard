b113a7732765c7719e882cf14e5a43975483c57f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-18 14:21:25 +0100

    .

--- pages/[username]/[project]/api-keys.tsx
+++ pages/[username]/[project]/api-keys.tsx
@@ -1,3 +1,4 @@
+import ApiKeysLoader from "@/components/loader/ApiKeys";
 import { axios } from "@/configs/axios";
 import { toastErrorConfig, toastSuccessConfig } from "@/configs/toast";
 import DashboardLayout from "@/layouts/DashboardLayout";
@@ -10,15 +11,18 @@ const ApiKeys = () => {
     sk: "",
     pk: "",
   });
+  const [loading, setLoading] = useState<boolean>(false);
 
   const fetchKeys = async () => {
+    setLoading(true);
     const flyName = window.location.pathname.split("/")[2];
     try {
       const { data } = await axios.get(`/api-keys?fly_name=${flyName}`);
       setKeys(data);
-      console.log(data);
+      setLoading(false);
     } catch (error) {
       setKeys({ sk: "", pk: "" });
+      setLoading(false);
     }
   };
 
@@ -28,21 +32,21 @@ const ApiKeys = () => {
 
   const { fly } = useFlyStore();
 
-  const [loading, setLoading] = useState<boolean>(false);
+  const [regenerating, setRegenerating] = useState<boolean>(false);
 
   const regenerateKeys = async () => {
-    setLoading(true);
+    setRegenerating(true);
     try {
       const res = await axios.put("/api-keys/create", {
         fly_id: fly.uuid,
       });
       setKeys(res.data.keys);
       toast("Keys successfully regenerated", toastSuccessConfig);
-      setLoading(false);
+      setRegenerating(false);
     } catch (error) {
       console.log(error);
       toast("Error regenerating keys", toastErrorConfig);
-      setLoading(false);
+      setRegenerating(false);
     }
   };
 
@@ -67,7 +71,10 @@ const ApiKeys = () => {
   };
 
   return (
-    <DashboardLayout>
+    <DashboardLayout
+      isChildLoading={loading}
+      childLoadingComponent={<ApiKeysLoader />}
+    >
       <h1 className="text-2xl font-semibold">API Keys</h1>
       <p className="mt-2 text-sm text-gray-400">
         View and manage your Uploadfly API keys
@@ -110,13 +117,13 @@ const ApiKeys = () => {
         <button
           className="flex items-center gap-2 bg-uf-light text-uf-dark px-5 py-3 rounded-md font-semibold disabled:opacity-70 disabled:cursor-not-allowed"
           onClick={regenerateKeys}
-          disabled={loading}
+          disabled={regenerating}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
             fill="currentColor"
-            className={`w-5 h-5 ${loading && "animate-spin"}`}
+            className={`w-5 h-5 ${regenerating && "animate-spin"}`}
           >
             <path
               fillRule="evenodd"
@@ -124,7 +131,7 @@ const ApiKeys = () => {
               clipRule="evenodd"
             />
           </svg>
-          {loading ? "Regenerating" : "Regenerate"}
+          {regenerating ? "Regenerating" : "Regenerate"}
         </button>
       </div>
     </DashboardLayout>
