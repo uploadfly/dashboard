0eb5add8cca1cb1f7032cb4082993288fc39431f
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-18 13:58:50 +0100

    omoo

--- layouts/DashboardLayout.tsx
+++ layouts/DashboardLayout.tsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import NavbarLoader from "@/components/loader/Navbar";
+import SidebarLoader from "@/components/loader/Sidebar";
 import { axios } from "@/configs/axios";
 import { useFlyStore } from "@/stores/flyStore";
 import { ReactNode, useEffect, useState } from "react";
@@ -31,31 +33,39 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
   }, []);
 
   return (
-    <div className="bg-uf-dark text-uf-light h-screen overflow-y-hidden">
-      {loading ? <>Loading navbar</> : <Navbar />}
-      <div className="h-screen flex w-full">
-        <div className="w-[20%] p-5 bg-[#050505] sticky top-1">
-          {loading ? <>Sidebar loading</> : <Sidebar />}
+    <>
+      {loading ? (
+        <div className="bg-uf-dark text-uf-light h-screen overflow-y-hidden">
+          {loading ? <NavbarLoader /> : <Navbar />}
+          <div className="h-screen flex w-full">
+            <div className="w-[20%] p-5 bg-[#050505] sticky top-1">
+              <Sidebar loading={loading} />
+            </div>
+            <div className="w-[80%] mt-5 px-10 overflow-y-scroll mb-20">
+              {loading ? <>Loading page</> : <>{children}</>}
+            </div>
+          </div>
         </div>
-        <div className="w-[80%] mt-5 px-10 overflow-y-scroll mb-20">
-          {loading ? (
-            <>Loading page</>
-          ) : (
-            <>
-              {status === 404 ? (
-                <h1>Not found</h1>
-              ) : status === 500 ? (
-                <>
-                  <h1>Internal server error</h1>
-                </>
-              ) : (
-                <>{children}</>
-              )}
-            </>
-          )}
+      ) : status === 404 ? (
+        <div className="bg-uf-dark text-uf-light flex items-center justify-center h-screen overflow-y-hidden">
+          <h1 className="text-center text-9xl font-bold">404</h1>
         </div>
-      </div>
-    </div>
+      ) : status === 500 ? (
+        <></>
+      ) : (
+        <div className="bg-uf-dark text-uf-light h-screen overflow-y-hidden">
+          {loading ? <NavbarLoader /> : <Navbar />}
+          <div className="h-screen flex w-full">
+            <div className="w-[20%] p-5 bg-[#050505] sticky top-1">
+              <Sidebar loading={loading} />
+            </div>
+            <div className="w-[80%] mt-5 px-10 overflow-y-scroll mb-20">
+              {loading ? <>Loading page</> : <>{children}</>}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
