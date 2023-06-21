1b83751db7a5d721442ac29d2a31b8604e69057e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-21 10:20:47 +0100

    space

--- components/loader/Navbar.tsx
+++ components/loader/Navbar.tsx
@@ -1,19 +1,17 @@
-import Link from "next/link";
-
 const NavbarLoader = () => {
   return (
     <div className="sticky top-0 z-50 bg-uf-dark">
-      <div className="flex items-center px-10 py-4  justify-between">
+      <div className="flex items-center px-10 py-4 justify-between">
         <h1 className="shiny-text text-xl">uploadfly</h1>
         <div className="flex gap-4 items-center">
           <div className="h-5 w-40 loading"></div>
-          <Link
+          <a
             href={"https://docs.uploadfly.io"}
             target="_blank"
             className="font-semibold text-gray-300"
           >
             Docs
-          </Link>
+          </a>
           <div className="w-10 h-10 loading rounded-full p-[2px]"></div>
         </div>
       </div>
