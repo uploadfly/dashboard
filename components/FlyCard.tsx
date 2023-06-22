65109fa928eb46990a70d3acf4d3c7505bac4968
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-22 16:23:46 +0100

    bro

--- components/FlyCard.tsx
+++ components/FlyCard.tsx
@@ -2,22 +2,27 @@ import { useFlyStore } from "@/stores/flyStore";
 import { useUserStore } from "@/stores/userStore";
 import moment from "moment";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import filesize from "file-size";
 
 const FlyCard = ({
   name,
   used,
   updated,
   uuid,
+  storage,
 }: {
   name: string;
   used: number;
   updated: string;
   uuid: string;
+  storage: number;
 }) => {
-  const usedStoragePercent = (used / 2048) * 100;
   const { user } = useUserStore();
   const { setFly } = useFlyStore();
+
+  const usedStorage = filesize(used).to("MB");
+  const usedStoragePercent =
+    Number(usedStorage) / Number(filesize(storage).to("MB"));
 
   return (
     <Link
@@ -28,7 +33,7 @@ const FlyCard = ({
       <h2 className="text-lg font-bold shiny-text">{name}</h2>
       <div className="my-10">
         <div className="flex justify-between mb-1">
-          <p>{used}MB</p>
+          <p>{usedStorage}MB</p>
           <p>2GB</p>
         </div>
         <div className="w-full h-2 bg-slate-200 rounded-full">
