22fe47e214c7d8e7e7e5ccc41016241df4c49dc2
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-22 15:16:03 +0100

    streak

---
+++ components/GH.tsx
@@ -0,0 +1,47 @@
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
+import dayjs from "dayjs";
+
+const ContributionGraph = ({ contributionData }: { contributionData: any }) => {
+  const startDate = dayjs().startOf("year");
+  const endDate = dayjs().endOf("year");
+
+  const getMaxCount = (data: any[]) => {
+    let maxCount = 0;
+
+    for (const value of data) {
+      if (value.count > maxCount) {
+        maxCount = value.count;
+      }
+    }
+
+    return maxCount;
+  };
+
+  return (
+    <div className="graph">
+      <CalendarHeatmap
+        startDate={startDate as any}
+        endDate={endDate as any}
+        values={contributionData}
+        classForValue={(value: any) => {
+          if (!value) {
+            return "color-scale-0";
+          }
+
+          const { count } = value;
+
+          const maxCount = getMaxCount(contributionData);
+
+          const range = Math.ceil(maxCount / 5);
+
+          const scaleIndex = Math.floor(count / range);
+
+          return `color-scale-${scaleIndex}`;
+        }}
+      />
+    </div>
+  );
+};
+
+export { ContributionGraph };
--- pages/[username]/[project]/index.tsx
+++ pages/[username]/[project]/index.tsx
@@ -1,12 +1,66 @@
+import { ContributionGraph } from "@/components/GH";
 import DashboardLayout from "@/layouts/DashboardLayout";
+import Link from "next/link";
 
 const Project = () => {
+  const generateContributionData = () => {
+    const data = [];
+    for (let i = 0; i < 365; i++) {
+      const date = new Date();
+      date.setDate(date.getDate() - i);
+      const count = Math.floor(Math.random() * 4);
+
+      data.push({ date, count });
+    }
+    return data;
+  };
+
+  const contributionData = generateContributionData();
+
+  const stats = [
+    {
+      title: "Total Files",
+      value: contributionData.reduce((a, b) => a + b.count, 0),
+    },
+    {
+      title: "Used storage",
+      value: "948 MB",
+    },
+  ];
+
   return (
     <DashboardLayout>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae,
-        dolorem!
-      </p>
+      <div className="border border-gray-700 p-5 rounded-md mb-5">
+        <h1 className="text-2xl font-semibold">Documentation</h1>
+        <p className="my-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <Link
+          href={"https://docs.uploadfly.io"}
+          target="_blank"
+          className="text-blue-500 font-semibold"
+        >
+          Go to docs
+        </Link>
+      </div>
+      <div className="flex gap-5">
+        {stats?.map((stat, index) => (
+          <div
+            className="border border-gray-700 p-5 rounded-md w-1/2"
+            key={index}
+          >
+            <h1 className="text-2xl font-semibold">{stat.title}</h1>
+            <p className="text-4xl mt-3">{stat.value}</p>
+          </div>
+        ))}
+      </div>
+      <div className="border border-gray-700 p-5 rounded-md mt-5">
+        <h1 className="text-2xl font-semibold">Upload streak</h1>
+        <p className="my-3 font-semibold">
+          {contributionData.reduce((a, b) => a + b.count, 0)} uploads in 2023
+        </p>
+        <ContributionGraph contributionData={contributionData} />
+      </div>
     </DashboardLayout>
   );
 };
