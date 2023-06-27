d87c5d4b5cbe07627af640041276d033ede9b5b2
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-27 13:03:35 +0100

    fetch real data

--- pages/api/analytics/index.ts
+++ pages/api/analytics/index.ts
@@ -3,6 +3,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 import jwt from "jsonwebtoken";
 
+interface FileUploadAnalytics {
+  date: string;
+  count: number;
+}
+
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const token = req.cookies.access_token;
@@ -39,14 +44,40 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     });
     if (!fly) return res.status(400).json({ message: "Invalid fly id" });
 
-    return res.status(200).json([
-      { date: "2023-06-21", count: 10 },
-      { date: "2023-06-22", count: 90 },
-      { date: "2023-06-23", count: 100 },
-      { date: "2023-06-24", count: 30 },
-      { date: "2023-06-25", count: 47 },
-      { date: "2023-06-26", count: 5 },
-    ]);
+    const fileUploads = await prisma.file.groupBy({
+      by: ["created_at"],
+      _count: { created_at: true }, // Use created_at instead of createdAt
+      where: {
+        fly_id: fly?.uuid,
+      },
+    });
+
+    const fileUploadAnalytics: FileUploadAnalytics[] = fileUploads.map(
+      (item) => ({
+        date: item.created_at.toISOString().split("T")[0],
+        count: item._count.created_at,
+      })
+    );
+
+    const dailyCounts: { [date: string]: number } = {};
+
+    fileUploadAnalytics.forEach((item) => {
+      const { date, count } = item;
+      if (dailyCounts[date]) {
+        dailyCounts[date] += count;
+      } else {
+        dailyCounts[date] = count;
+      }
+    });
+
+    const result: FileUploadAnalytics[] = Object.keys(dailyCounts).map(
+      (date) => ({
+        date,
+        count: dailyCounts[date],
+      })
+    );
+
+    return res.status(200).json(result);
   } catch (error) {
     console.log(error);
 
