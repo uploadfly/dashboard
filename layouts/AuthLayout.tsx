3e4856ac41436c01723400aebb72b3b8506f2f5a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-06-28 14:08:57 +0100

    added logo and fixed transform

--- layouts/AuthLayout.tsx
+++ layouts/AuthLayout.tsx
@@ -57,11 +57,9 @@ const AuthLayout = ({
       <div
         className={`abosolute top-0 w-full h-full bg-uf-dark flex items-center justify-center`}
       >
-        <div className="flex flex-col relativeitems-center justify-center bg-[#050505] w-[35%] h-full">
-          <div
-            className="animate-breath absolute pointer-events-none flex items-center flex-wrap w-56 justify-center translate-x-[50%] top-[50%] translate-y-[-50%]
-      "
-          >
+        <div className="bg-[#050505] w-[35%] h-full flex items-center justify-center">
+          <img src="/logo.svg" alt="" className="w-40 opacity-70" />
+          <div className="animate-breath absolute pointer-events-none flex items-center flex-wrap w-56 justify-center translate-x-[0%] top-[50%] -translate-y-[50%]">
             <div className="h-28 w-28 rounded-full blur-2xl bg-[#0083cb]"></div>
             <div className="h-28 w-28 rounded-full blur-2xl bg-[#a06eac]"></div>
             <div className="h-28 w-28 rounded-full blur-2xl bg-[#ffb564]"></div>
