70b5816947e12b639dcece03b5c5d7c382d0a9b6
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-02 14:09:03 +0100

    changed loading icon and behaviour

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -5,6 +5,7 @@ import toast from "react-hot-toast";
 import OTPInput from "react-otp-input";
 import { axiosAuth } from "@/configs/axios";
 import { useRouter } from "next/router";
+import { RiLoader5Fill } from "react-icons/ri";
 
 const Signup = () => {
   const [email, setEmail] = useState<string>("");
@@ -152,24 +153,9 @@ const Signup = () => {
             />
           </>
         )}
-        <button className="w-[380px] uf-gradient rounded-md py-2 h-10 flex items-center justify-center text-[#1e1e1e] font-bold hover:scale-105 transition-all">
+        <button className="w-[380px] bg-uf-accent rounded-md py-2 h-10 flex items-center justify-center text-[#1e1e1e] font-bold hover:scale-105 transition-all">
           {loading ? (
-            <span className="animate-slide w-1/2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-7 h-7 text-uf-dark"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-                />
-              </svg>
-            </span>
+            <RiLoader5Fill className="animate-spin text-2xl" />
           ) : (
             <span className="shadow-2xl">
               {showOtpInput ? "Complete signup" : "Continue"}
