948d53a72c80c46b8369029c337060f112941664
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-02 12:56:36 +0100

    added favivon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,22 @@
 import AuthProvider from "@/provider/Auth";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
-      <Toaster />
-      <Component {...pageProps} />
-    </AuthProvider>
+    <>
+      <Head>
+        <link
+          rel="shortcut icon"
+          href="https://cdn.uploadfly.cloud/xCnc4L/favicon-orange-mPuTin-huL.png"
+        />
+      </Head>
+      <AuthProvider>
+        <Toaster />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </>
   );
 }
