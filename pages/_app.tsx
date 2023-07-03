f90d0403548fa2e05947a254374348410fcb467c
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-03 12:22:34 +0100

    added page loading progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,18 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
+import { Router } from "next/router";
+import Progress from "@badrap/bar-of-progress";
+
+const progress = new Progress({
+  className: "bar-of-progress",
+  color: "#F35815",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", () => progress.start());
+Router.events.on("routeChangeComplete", () => progress.finish());
+Router.events.on("routeChangeError", () => progress.finish());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
