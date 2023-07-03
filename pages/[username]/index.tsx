a165234246a69ea685b18ecde59e22bd86ca8ecb
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-03 11:11:12 +0100

    molved "lauch" button to the top

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -6,6 +6,7 @@ import { useUserStore } from "@/stores/userStore";
 import Head from "next/head";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import { HiOutlineRocketLaunch } from "react-icons/hi2";
 
 const DashboardIndex = () => {
   const { user } = useUserStore();
@@ -70,8 +71,21 @@ const DashboardIndex = () => {
                   </Link>
                 </>
               ) : (
-                <div className="flex flex-col items-center">
-                  <div className="grid gap-4 items-center justify-center mt-8 grid-cols-3">
+                <div className="flex flex-col items-center w-screen px-10">
+                  <div className="mt-8 flex justify-between w-full">
+                    <input
+                      type="text"
+                      className="bg-transparent border border-gray-800 rounded-md pl-3 w-[450px] outline-uf-accent"
+                      placeholder="Search for a fly"
+                    />
+                    <Link href={"/launch"} className="">
+                      <button className="flex gap-2 bg-uf-light text-uf-dark rounded-md py-2 px-4 w-full items-center justify-center font-bold hover:scale-105 transition-all disabled:opacity-50 disabled:cursor-not-allowed">
+                        <HiOutlineRocketLaunch className="text-xl" />
+                        Create a new fly
+                      </button>
+                    </Link>
+                  </div>
+                  <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 w-full mt-12">
                     {flies.map((fly) => {
                       return (
                         <FlyCard
@@ -85,27 +99,6 @@ const DashboardIndex = () => {
                       );
                     })}
                   </div>
-                  <center className="mt-8 w-[250px]">
-                    <Link href={"/launch"} className="w-full">
-                      <button className="flex gap-2 bg-uf-light text-uf-dark rounded-md py-2 w-full items-center justify-center font-bold hover:scale-105 transition-all disabled:opacity-50 disabled:cursor-not-allowed">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className={`w-6 h-6 ${loading && "animate-spin"}`}
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.59 14.37a6 6 0 01-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 006.16-12.12A14.98 14.98 0 009.631 8.41m5.96 5.96a14.926 14.926 0 01-5.841 2.58m-.119-8.54a6 6 0 00-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 00-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 01-2.448-2.448 14.9 14.9 0 01.06-.312m-2.24 2.39a4.493 4.493 0 00-1.757 4.306 4.493 4.493 0 004.306-1.758M16.5 9a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0z"
-                          />
-                        </svg>
-                        Create a new fly
-                      </button>
-                    </Link>
-                  </center>
                 </div>
               )}
             </div>
