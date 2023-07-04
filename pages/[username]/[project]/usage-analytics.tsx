bba7c5173886edce08ec31ecc8b537a23a14db75
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-04 17:37:10 +0100

    initial commit

---
+++ pages/[username]/[project]/usage-analytics.tsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "@/layouts/DashboardLayout";
+
+const UsageAndAnalytics = () => {
+  return (
+    <DashboardLayout pageName="Usage & Analytics">
+      <div>UsageAndAnalytics</div>
+    </DashboardLayout>
+  );
+};
+
+export default UsageAndAnalytics;
