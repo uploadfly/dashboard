5038f4e369eebf50cb4c111a0bce837ab2a0af0c
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-04 18:07:20 +0100

    added settings subpages

--- pages/[username]/[project]/settings.tsx
+++ pages/[username]/[project]/settings.tsx
@@ -1,8 +1,40 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
-import React from "react";
+import { useFlyStore } from "@/stores/flyStore";
+import Link from "next/link";
 
 const Settings = () => {
-  return <DashboardLayout pageName="Settings">Settings</DashboardLayout>;
+  const { fly } = useFlyStore();
+  const subSettingsPages = [
+    {
+      title: "General",
+      route: "/",
+    },
+    {
+      title: "Billing and Plans",
+      route: "/billing-plans",
+    },
+    {
+      title: "Advanced",
+      route: "/advanced",
+    },
+  ];
+  return (
+    <DashboardLayout pageName="Settings">
+      <div className="">
+        <div className={`flex flex-col gap-5`}>
+          {subSettingsPages.map((page) => (
+            <Link
+              href={`${window.location.origin}/${fly?.name}/settings${page.route}`}
+              key={page.title}
+            >
+              {page.title}
+            </Link>
+          ))}
+        </div>
+        <div className=""></div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Settings;
