3ad6c0b36365e77048316514dc14d4c7da44ad11
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-05 11:05:44 +0100

    created extended request

--- interfaces.ts
+++ interfaces.ts
@@ -9,8 +9,21 @@ export interface FileProps {
   uploaded: Date;
 }
 
-export interface AuthenticatedRequest extends NextApiRequest {
-  user?: {
-    uuid: string;
-  };
+export interface ExtendedRequest extends NextApiRequest {
+  user: UserModel;
+}
+
+export interface UserModel {
+  id: number;
+  email: string;
+  username: string;
+  password?: string;
+  otp?: string;
+  otp_expiry?: Date;
+  email_verified: boolean;
+  created_at: Date;
+  updated_at: Date;
+  uuid: string;
+  auth_method?: string;
+  github_id?: number;
 }
