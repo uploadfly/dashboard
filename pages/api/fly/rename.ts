1e8f9fd687c26c87ee179a8f817c40772d386952
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-05 12:06:52 +0100

    try catch

--- pages/api/fly/rename.ts
+++ pages/api/fly/rename.ts
@@ -5,44 +5,55 @@ import { NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
-  const fly_id = req.body.fly_id;
-  const name = req.body.name;
+  try {
+    const fly_id = req.body.fly_id;
+    const name = req.body.name;
 
-  if (!fly_id) {
-    res.status(400).json({ message: "Fly ID is missing in request" });
-  }
+    if (!fly_id) {
+      res.status(400).json({ message: "Fly ID is missing in request" });
+      return;
+    }
 
-  const data = {
-    uuid: fly_id,
-    user_id: req.user.uuid,
-  };
-  const fly = await prisma.fly.findFirst({
-    where: data,
-  });
+    if (!name) {
+      res.status(400).json({ message: "New name is missing in request" });
+      return;
+    }
 
-  if (!fly) {
-    res.status(404).json({ message: "Fly not found" });
-    return;
-  }
-
-  const flyNameRegex = /^(?!-)(?!.*--)[a-z0-9-]{3,100}(?<!-)$/i;
-
-  if (!flyNameRegex.test(name)) {
-    res.status(400).json({
-      message:
-        "Fly names can contain up 100 alphanumeric lowercase characters. Hyphens can be used between the name but never at the start or end.",
+    const fly = await prisma.fly.findFirst({
+      where: {
+        uuid: fly_id,
+        user_id: req.user.uuid,
+      },
     });
-    return;
+
+    if (!fly) {
+      res.status(404).json({ message: "Fly not found" });
+      return;
+    }
+
+    const flyNameRegex = /^(?!-)(?!.*--)[a-z0-9-]{3,100}(?<!-)$/i;
+
+    if (!flyNameRegex.test(name)) {
+      res.status(400).json({
+        message:
+          "Fly names can contain up 100 alphanumeric lowercase characters. Hyphens can be used between the name but never at the start or end.",
+      });
+      return;
+    }
+
+    await prisma.fly.update({
+      where: {
+        uuid: fly_id,
+      },
+      data: {
+        name,
+      },
+    });
+
+    return res.status(200).json({ message: "Fly name updated" });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
   }
-
-  await prisma.fly.update({
-    where: data,
-    data: {
-      name,
-    },
-  });
-
-  res.status(200).json({ message: "Fly name updated" });
 };
 
 const middlewareChain = allowMethods(["PUT"])(
