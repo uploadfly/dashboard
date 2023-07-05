cf53b3895feb295157336b91b6333945e3ae43a5
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-05 09:38:16 +0100

    wrapped in settings layout

--- pages/[username]/[project]/settings/index.tsx
+++ pages/[username]/[project]/settings/index.tsx
@@ -1,49 +1,7 @@
-import DashboardLayout from "@/layouts/DashboardLayout";
-import { useFlyStore } from "@/stores/flyStore";
-import { useUserStore } from "@/stores/userStore";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { ReactNode, useEffect, useState } from "react";
+import SettingsLayout from "@/layouts/SettingsLayout";
 
-const Settings = ({ children }: { children: ReactNode }) => {
-  const { fly } = useFlyStore();
-  const subSettingsPages = [
-    {
-      title: "General",
-      route: "/",
-    },
-    {
-      title: "Billing and Plans",
-      route: "/billing-plans",
-    },
-    {
-      title: "Advanced",
-      route: "/advanced",
-    },
-  ];
-
-  const [baseRoute, setBaseRoute] = useState("");
-  const { user } = useUserStore();
-
-  useEffect(() => {
-    setBaseRoute(
-      `${window?.location.origin}/${user?.username}/${fly?.name}/settings`
-    );
-  }, [fly, user]);
-  return (
-    <DashboardLayout pageName="Settings">
-      <div className="flex">
-        <div className={`flex flex-col gap-5`}>
-          {subSettingsPages.map((page) => (
-            <Link href={`${baseRoute}${page.route}`} key={page.title}>
-              {page.title}
-            </Link>
-          ))}
-        </div>
-        <div className="ml-28">{children}</div>
-      </div>
-    </DashboardLayout>
-  );
+const Settings = () => {
+  return <SettingsLayout>Settings</SettingsLayout>;
 };
 
 export default Settings;
