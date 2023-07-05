c951a3d07929392b6b9c688bfe43dd59fae9d94c
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-05 12:35:58 +0100

    initial commit

---
+++ pages/[username]/[project]/settings/billing-plans.tsx
@@ -0,0 +1,11 @@
+import SettingsLayout from "@/layouts/SettingsLayout";
+
+const BillingAndPlans = () => {
+  return (
+    <SettingsLayout>
+      <h1>Biiling and Plans</h1>
+    </SettingsLayout>
+  );
+};
+
+export default BillingAndPlans;
