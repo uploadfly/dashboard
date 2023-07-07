ce8e55bd7a2bb9eb53752a52ec49f03ec894de35
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-07 23:07:13 +0100

    added options icon and file size

--- components/File.tsx
+++ components/File.tsx
@@ -1,4 +1,6 @@
+import fileSize from "file-size";
 import { BsCheckLg } from "react-icons/bs";
+import { HiEllipsisVertical } from "react-icons/hi2";
 
 const File = ({
   name,
@@ -19,13 +21,18 @@ const File = ({
         {name}
       </td>
       <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-        {type}
+        2 hours ago
       </td>
       <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-        {size}
+        PNG
       </td>
       <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
-        button
+        {fileSize(size).human("si")}
+      </td>
+      <td>
+        <button>
+          <HiEllipsisVertical />
+        </button>
       </td>
     </tr>
   );
