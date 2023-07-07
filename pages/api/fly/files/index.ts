2d82bd78353c3360e1d5f0912be5df2409794030
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-07 22:29:18 +0100

    created endpoint to list files

---
+++ pages/api/fly/files/index.ts
@@ -0,0 +1,53 @@
+import { ExtendedRequest } from "@/interfaces";
+import authenticateToken from "@/middleware/auth";
+import prisma from "@/prisma";
+import { NextApiResponse } from "next";
+import { allowMethods } from "next-method-guard";
+
+const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
+  const fly_id = req.query.fly_id;
+
+  const fly = await prisma.fly.findUnique({
+    where: {
+      uuid: fly_id as string,
+    },
+  });
+
+  if (!fly) {
+    res.status(400).json({
+      message: "Invalid fly ID",
+    });
+    return;
+  }
+
+  if (fly.user_id !== req.user.uuid) {
+    res.status(400).json({
+      message: "Invalid fly ID",
+    });
+    return;
+  }
+
+  const files = await prisma.file.findMany({
+    where: {
+      fly_id: fly.uuid,
+    },
+    select: {
+      path: true,
+      size: true,
+      url: true,
+      created_at: true,
+    },
+  });
+
+  res.status(200).json({
+    message: "Files retrieved successfully",
+    files,
+  });
+};
+
+const middlewareChain = allowMethods(["POST"])(
+  (req: ExtendedRequest, res: NextApiResponse) =>
+    authenticateToken(req, res, () => handler(req, res))
+);
+
+export default middlewareChain;
