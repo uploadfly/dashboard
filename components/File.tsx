2be4d62bd217eb6dcb8a87f94020c3b5783893e1
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-07 23:24:14 +0100

    changed styles

--- components/File.tsx
+++ components/File.tsx
@@ -23,23 +23,19 @@ const File = ({
   };
 
   return (
-    <tr>
-      <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+    <tr className="hover:bg-slate-800/30">
+      <td className="border-b border-slate-700 p-4  text-slate-400">
         {truncate(name)}
       </td>
-      <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
+      <td className="border-b border-slate-700 p-4 text-slate-400">
         2 hours ago
       </td>
-      <td className="border-b border-slate-100 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400">
-        PNG
-      </td>
-      <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400">
+      <td className="border-b border-slate-700 p-4 text-slate-400">PNG</td>
+      <td className="border-b border-slate-700 p-4 text-slate-400">
         {fileSize(size).human("si")}
       </td>
-      <td>
-        <button>
-          <HiEllipsisVertical />
-        </button>
+      <td className="border-b border-slate-700 p-4 text-slate-400 text-xl">
+        <HiEllipsisVertical />
       </td>
     </tr>
   );
