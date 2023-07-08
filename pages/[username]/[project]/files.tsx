b39a825c9bc6a34a6a42a8d2c012c28aa54286e0
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-08 22:45:54 +0100

    removed unused

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -55,27 +55,6 @@ const Files = () => {
     <DashboardLayout pageName="Files">
       <div className="flex gap-5">
         <div className="h-full w-full">
-          <div className="w-full h-16 sticky bg-[#050505] z-10 flex items-center px-2 mb-3">
-            <div className="flex justify-between w-full items-center">
-              <div className="">
-                <p>{files.length} files</p>
-                <p>{files.reduce((acc, curr) => acc + curr.size, 0)} bytes</p>
-              </div>
-              <div className="flex items-center gap-4">
-                <button></button>
-                <button
-                  onClick={() => selection("all")}
-                  className="border border-gray-50 py-2 px-4 font-semibold rounded-md"
-                >
-                  Select all
-                </button>
-                <button className="bg-uf-light text-uf-dark flex items-center py-2 px-4 font-semibold gap-2 rounded-md">
-                  <CgSoftwareUpload className="text-xl" />
-                  Upload
-                </button>
-              </div>
-            </div>
-          </div>
           <table className="border-collapse table-auto w-full text-sm">
             <thead>
               <tr>
