3e9a44ef126e7e9d8440b0c081f293cf0c8a5d9e
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-09 16:03:35 +0100

    .

--- components/SelectedFile.tsx
+++ components/SelectedFile.tsx
@@ -3,6 +3,9 @@ import fileSize from "file-size";
 import moment from "moment";
 import { truncate } from "./File";
 import { IoCopyOutline } from "react-icons/io5";
+import { copyToClipboard } from "@/pages/[username]/[project]/api-keys";
+import { toast } from "react-hot-toast";
+import { toastSuccessConfig } from "@/configs/toast";
 
 const Flex = ({ title, value }: { title: string; value: string }) => {
   return (
@@ -19,8 +22,7 @@ const SelectedFile = ({ files }: { files: FileProps[] }) => {
     <div className="w-full h-full p-3 rounded-md">
       {files.length === 1 ? (
         <div className="flex gap-5 flex-col">
-          {file && <h2 className="text-xl font-semibold mb-3">File details</h2>}
-
+          {file && <h2 className="text-xl font-semibold">File details</h2>}
           <Flex title="Name" value={truncate(file.name, 45)} />
           <Flex title="Size" value={fileSize(file.size).human("si")} />
           <Flex
@@ -32,14 +34,24 @@ const SelectedFile = ({ files }: { files: FileProps[] }) => {
             <h3 className="font-semibold">URL</h3>
             <div className="flex gap-3">
               <h3>{truncate(file.url, 40)}</h3>
-              <button>
+              <button
+                onClick={() => {
+                  copyToClipboard(file.url);
+                  toast("Copied to clipboard", {
+                    duration: 500,
+                  });
+                }}
+                className="hover:text-uf-accent transition-colors"
+              >
                 <IoCopyOutline />
               </button>
             </div>
           </div>
         </div>
       ) : (
-        <></>
+        <>
+          <p className="">Multiple files selected</p>
+        </>
       )}
     </div>
   );
