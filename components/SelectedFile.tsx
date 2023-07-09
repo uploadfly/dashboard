1804d5f4a714c9b5e5bb685f5404d0ded8d1d789
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-09 15:56:22 +0100

    initial commit

---
+++ components/SelectedFile.tsx
@@ -0,0 +1,48 @@
+import { FileProps } from "@/interfaces";
+import fileSize from "file-size";
+import moment from "moment";
+import { truncate } from "./File";
+import { IoCopyOutline } from "react-icons/io5";
+
+const Flex = ({ title, value }: { title: string; value: string }) => {
+  return (
+    <div className="">
+      <h3 className="font-semibold">{title}</h3>
+      <h3>{value}</h3>
+    </div>
+  );
+};
+
+const SelectedFile = ({ files }: { files: FileProps[] }) => {
+  const file = files?.[0];
+  return (
+    <div className="w-full h-full p-3 rounded-md">
+      {files.length === 1 ? (
+        <div className="flex gap-5 flex-col">
+          {file && <h2 className="text-xl font-semibold mb-3">File details</h2>}
+
+          <Flex title="Name" value={truncate(file.name, 45)} />
+          <Flex title="Size" value={fileSize(file.size).human("si")} />
+          <Flex
+            title="Date uploaded"
+            value={moment(file.created_at).calendar()}
+          />
+          <Flex title="Mime type" value={file.type} />
+          <div className="">
+            <h3 className="font-semibold">URL</h3>
+            <div className="flex gap-3">
+              <h3>{truncate(file.url, 40)}</h3>
+              <button>
+                <IoCopyOutline />
+              </button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default SelectedFile;
