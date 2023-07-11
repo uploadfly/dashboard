34d9dc45444027b05d5803d3ceb48489a2336043
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-11 19:21:14 +0100

    initial commit

--- components/ContextMenu.tsx
+++ components/ContextMenu.tsx
@@ -1,11 +1,19 @@
 import { Content, Portal, Root, Trigger } from "@radix-ui/react-popover";
+import { HiEllipsisVertical } from "react-icons/hi2";
 
 const ContextMenu = () => {
   return (
     <Root>
-      <Trigger asChild></Trigger>
+      <Trigger asChild>
+        <HiEllipsisVertical />
+      </Trigger>
       <Portal>
-        <Content></Content>
+        <Content>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+          aspernatur quo omnis dicta minima illum fuga nam repudiandae. Velit
+          earum laborum error blanditiis excepturi porro provident. Reiciendis
+          cumque molestiae vel.
+        </Content>
       </Portal>
     </Root>
   );
