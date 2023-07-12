059b7d6e61307d625bf9a81bd2f62c9d05ffeb50
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-13 00:02:42 +0100

    added delete button and text

--- pages/[username]/[project]/settings/advanced.tsx
+++ pages/[username]/[project]/settings/advanced.tsx
@@ -3,7 +3,18 @@ import SettingsLayout from "@/layouts/SettingsLayout";
 const Advanced = () => {
   return (
     <SettingsLayout>
-      <h1>Advanced</h1>
+      <div className="flex items-center gap-32">
+        <div className="">
+          <h1 className="font-semibold">Delete fly</h1>
+          <p className="text-sm text-gray-400">
+            This cannot be undone or restored. Make sure you really want to do
+            this.
+          </p>
+        </div>
+        <button className="border border-red-600 px-16 py-2 rounded-md font-semibold hover:bg-red-600 transition-colors">
+          Delete
+        </button>
+      </div>
     </SettingsLayout>
   );
 };
