c994e73ace2c15bcc634d91ee930bd88868c52af
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-22 13:18:58 +0100

    linked to docs website

--- components/NoFiles.tsx
+++ components/NoFiles.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { HiOutlineExternalLink } from "react-icons/hi";
 
 const NoFiles = () => {
@@ -7,9 +8,11 @@ const NoFiles = () => {
       <p className="text mb-4 w-[450px] text-center">
         Check out the docs to learn how to upload files.
       </p>
-      <button className="flex gap-2 bg-uf-light text-uf-dark items-center px-3 font-semibold py-2 rounded-md hover:bg-uf-accent hover:text-uf-dark transition-colors">
-        Go to docs <HiOutlineExternalLink />
-      </button>
+      <Link href={"https://docs.uploadfly.cloud"} target="_blank">
+        <button className="flex gap-2 bg-uf-light text-uf-dark items-center px-3 font-semibold py-2 rounded-md hover:bg-uf-accent hover:text-uf-dark transition-colors">
+          Go to docs <HiOutlineExternalLink />
+        </button>
+      </Link>
     </div>
   );
 };
