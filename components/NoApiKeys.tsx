e431dc0da9257357e9ff8ee0df380558c49b86ed
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-22 13:33:43 +0100

    added onClick prop

--- components/NoApiKeys.tsx
+++ components/NoApiKeys.tsx
@@ -1,6 +1,6 @@
 import { HiPlus } from "react-icons/hi2";
 
-const NoApiKeys = () => {
+const NoApiKeys = ({ onClick }: { onClick: () => void }) => {
   return (
     <div className="flex items-center justify-center flex-col mt-20">
       <h1 className="text-2xl font-semibold mb-2">
@@ -10,7 +10,10 @@ const NoApiKeys = () => {
         API Keys are randomly generated alpha-numeric strings that are used to
         interact with Uploadfly services.
       </p>
-      <button className="flex gap-2 bg-uf-light text-uf-dark items-center px-3 font-semibold py-2 rounded-md hover:bg-uf-accent hover:text-uf-dark transition-colors">
+      <button
+        className="flex gap-2 bg-uf-light text-uf-dark items-center px-3 font-semibold py-2 rounded-md hover:bg-uf-accent hover:text-uf-dark transition-colors"
+        onClick={onClick}
+      >
         <HiPlus /> Create an API key
       </button>
     </div>
