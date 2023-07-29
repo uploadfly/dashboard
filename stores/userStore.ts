0e2750cf96b5b2ef33c6879877dfe9ea08ca4530
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-29 03:49:03 +0100

    updated styles

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -7,7 +7,7 @@ interface UserStore {
 }
 
 export const useUserStore = create<UserStore>((set) => ({
-  user: null,
+  user: { username: "akinkunmioye42", email: "akinkunmioye42@gmail.com" },
   setUser: async () => {
     if (!useUserStore.getState().user) {
       try {
