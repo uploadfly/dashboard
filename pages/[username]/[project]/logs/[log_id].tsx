4d788d3df295b83bc2b232d1aaa06c57d06bc06a
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-29 01:17:33 +0100

    fetched log details

--- pages/[username]/[project]/logs/[log_id].tsx
+++ pages/[username]/[project]/logs/[log_id].tsx
@@ -1,7 +1,27 @@
 import DashboardLayout from "@/layouts/DashboardLayout";
+import { useFlyStore } from "@/stores/flyStore";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { axios } from "@/configs/axios";
 
 const Log = () => {
-  return <DashboardLayout pageName="Log Details">Log details</DashboardLayout>;
+  const { fly } = useFlyStore();
+  const router = useRouter();
+  const [log, setLog] = useState({});
+  useEffect(() => {
+    if (router.query.log_id) {
+      (async () => {
+        const log_id = router.query.log_id;
+
+        const { data } = await axios(
+          `/log?log_id=${log_id}&fly_id=${fly.uuid}`
+        );
+        console.log(data);
+      })();
+    }
+  }, [router.query.log_id]);
+
+  return <DashboardLayout pageName="Log Details">deets</DashboardLayout>;
 };
 
 export default Log;
