d76d80d9363fec5769a56c6013138bd7ba3cabf4
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-29 03:48:49 +0100

    updated styles

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -65,7 +65,7 @@ const NavLinks = ({ loading }: { loading: boolean }) => {
   const { user } = useUserStore();
 
   return (
-    <div className="px-10">
+    <div className="lg:px-10 px-5 w-full overflow-x-auto">
       <div className={loading ? "pointer-events-none" : "flex gap-12 mt-3"}>
         {links.map((link, i) => {
           const href = `/${user?.username}/${flyName}${link.path}`;
@@ -77,14 +77,14 @@ const NavLinks = ({ loading }: { loading: boolean }) => {
               legacyBehavior
             >
               <a
-                className={`flex items-center ${
+                className={`flex whitespace-nowrap items-center ${
                   currentRoute === link.path
                     ? "border-uf-accent"
                     : "border-transparent hover:border-uf-accent/40"
-                } pb-2 px-3 border-b-4 transition-colors duration-500`}
+                } pb-2 lg:px-4  border-b-4 transition-colors duration-500`}
               >
                 <div className="">{link.icon}</div>
-                <span className="ml-2">{link.name}</span>
+                <span className="ml-2 text-center">{link.name}</span>
               </a>
             </Link>
           );
