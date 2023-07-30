a54b4b96afec8c643c98f672480d4ff91682c9d5
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-30 10:09:01 +0100

    added close file details sidebar button

--- components/SelectedFile.tsx
+++ components/SelectedFile.tsx
@@ -3,9 +3,10 @@ import fileSize from "file-size";
 import moment from "moment";
 import { truncate } from "./File";
 import { IoCopyOutline } from "react-icons/io5";
-import { copyToClipboard } from "@/pages/[username]/[project]/api-keys";
 import { toast } from "react-hot-toast";
 import { toastSuccessConfig } from "@/configs/toast";
+import { RiCloseFill } from "react-icons/ri";
+import { copyToClipboard } from "@/utils/copyToClipboard";
 
 const Flex = ({ title, value }: { title: string; value: string }) => {
   return (
@@ -16,13 +17,26 @@ const Flex = ({ title, value }: { title: string; value: string }) => {
   );
 };
 
-const SelectedFile = ({ file }: { file: FileProps | null | undefined }) => {
+const SelectedFile = ({
+  file,
+  onClick,
+}: {
+  file: FileProps | null | undefined;
+  onClick: () => void;
+}) => {
   // const file = files?.[0];
   return (
     <div className="w-full h-full p-3 rounded-md">
       {file && (
         <div className="flex gap-5 flex-col">
-          {file && <h2 className="text-xl font-semibold">File details</h2>}
+          {file && (
+            <div className="flex items-center justify-between">
+              <h2 className="text-xl font-semibold">File details</h2>
+              <button onClick={onClick}>
+                <RiCloseFill className="text-3xl" />
+              </button>
+            </div>
+          )}
           <Flex title="Name" value={truncate(file.name, 45)} />
           <Flex title="Size" value={fileSize(file.size).human("si")} />
           <Flex
