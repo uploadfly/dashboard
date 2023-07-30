a776af0bf7fa537b2511ed62363fbe00d8c3ee57
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-30 10:38:08 +0100

    removed dummy data

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -8,40 +8,7 @@ import { useFlyStore } from "@/stores/flyStore";
 import { useEffect, useState } from "react";
 
 const Files = () => {
-  const [files, setFiles] = useState<FileProps[]>([
-    {
-      path: "n5oZ8g/peLGmSveSCNt5Rd9.docx",
-      size: 20330,
-      url: "https://d19172n4opxkkj.cloudfront.net/n5oZ8g/peLGmSveSCNt5Rd9.docx",
-      created_at: "2023-07-28T09:53:58.507Z",
-      name: "peLGmSveSCNt5Rd9",
-      type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    },
-    {
-      path: "n5oZ8g/hCjYO23iq4kfP8UW.docx",
-      size: 20330,
-      url: "https://d19172n4opxkkj.cloudfront.net/n5oZ8g/hCjYO23iq4kfP8UW.docx",
-      created_at: "2023-07-28T09:53:58.509Z",
-      name: "hCjYO23iq4kfP8UW",
-      type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-    },
-    {
-      path: "n5oZ8g/Kb2MDn68eetkw5h0.pdf",
-      size: 293021,
-      url: "https://d19172n4opxkkj.cloudfront.net/n5oZ8g/Kb2MDn68eetkw5h0.pdf",
-      created_at: new Date("2023-07-28T10:05:58.868Z"),
-      name: "Kb2MDn68eetkw5h0",
-      type: "application/pdf",
-    },
-    {
-      path: "n5oZ8g/JBfEIAOYE31D0Jjw.pdf",
-      size: 293021,
-      url: "https://d19172n4opxkkj.cloudfront.net/n5oZ8g/JBfEIAOYE31D0Jjw.pdf",
-      created_at: new Date("2023-07-28T10:05:58.923Z"),
-      name: "JBfEIAOYE31D0Jjw",
-      type: "application/pdf",
-    },
-  ]);
+  const [files, setFiles] = useState<FileProps[]>([]);
 
   // const [selectedFiles, setSelectedFiles] = useState<FileProps[]>([]);
   const [selectedFile, setSelectedFile] = useState<FileProps | null>();
@@ -56,20 +23,20 @@ const Files = () => {
 
   const { fly } = useFlyStore();
 
-  // const getFiles = async (fly_id: string) => {
-  //   try {
-  //     const { data } = await axios(`/fly/files?fly_id=${fly_id}`);
-  //     setFiles(data?.files);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
+  const getFiles = async (fly_id: string) => {
+    try {
+      const { data } = await axios(`/fly/files?fly_id=${fly_id}`);
+      setFiles(data?.files);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-  // useEffect(() => {
-  //   if (fly?.uuid) {
-  //     getFiles(fly?.uuid);
-  //   }
-  // }, [fly?.uuid]);
+  useEffect(() => {
+    if (fly?.uuid) {
+      getFiles(fly?.uuid);
+    }
+  }, [fly?.uuid]);
 
   const tableHeads = ["Name", "Date uploaded", "Mime type", "File size"];
 
