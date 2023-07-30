c538a470dd46e591e76979d7604b115a3a228a97
akinloluwami <akinkunmiisrael41@gmail.com>
2023-07-30 11:23:45 +0100

    .

--- components/NoLogs.tsx
+++ components/NoLogs.tsx
@@ -6,8 +6,8 @@ const NoLogs = () => {
     <div className="flex items-center justify-center flex-col mt-20">
       <h1 className="text-2xl font-semibold mb-2">No logs yet.</h1>
       <p className="text mb-4 lg:w-[450px] text-center">
-        Your logs will appear hear when you make API requests Check out the docs
-        to learn about Uploadfly API endpoints.
+        Your logs will appear hear when you make API requests. <br /> Check out
+        the docs to learn about Uploadfly API endpoints.
       </p>
       <Link href={"https://docs.uploadfly.cloud"} target="_blank">
         <button className="flex gap-2 bg-uf-light text-uf-dark items-center px-3 font-semibold py-2 rounded-md hover:bg-uf-accent hover:text-uf-dark transition-colors">
