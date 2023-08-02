8494d4e73a8b8c45a6d4d82a197773ae1f9760c1
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-02 05:19:00 +0100

    fixed navbar loading

--- components/loader/Navbar.tsx
+++ components/loader/Navbar.tsx
@@ -1,21 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import { HiOutlineExternalLink } from "react-icons/hi";
+
 const NavbarLoader = () => {
   return (
     <div className="sticky top-0 z-50 bg-uf-dark">
       <div className="flex items-center px-10 py-4 justify-between">
-        <h1 className="shiny-text text-xl">uploadfly</h1>
+        <div className="flex items-center gap-2">
+          <Image
+            src={"/logo.svg"}
+            width={40}
+            height={40}
+            alt="Uploadfly logo"
+          />
+          <div className="w-32 h-4 loading-animation"></div>
+        </div>
         <div className="flex gap-4 items-center">
-          <div className="h-5 w-40 loading"></div>
-          <a
-            href={"https://docs.uploadfly.io"}
+          <Link
+            href={"https://docs.uploadfly.cloud"}
             target="_blank"
-            className="font-semibold text-gray-300"
+            className="hover:text-uf-accent transition-colors flex items-center gap-1"
           >
             Docs
-          </a>
-          <div className="w-10 h-10 loading rounded-full p-[2px]"></div>
+            <HiOutlineExternalLink />
+          </Link>
+          <div className="w-10 h-10 loading-animation rounded-full p-[2px]"></div>
         </div>
       </div>
-      <div className="uf-gradient w-full h-[2px] opacity-70"></div>
+      <div className="bg-uf-accent w-full h-[2px] opacity-50"></div>
     </div>
   );
 };
