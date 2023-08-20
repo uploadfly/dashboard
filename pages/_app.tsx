8bad28d1a93f44dc9e61380635bd0ed8e41c9f32
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-20 12:08:10 +0100

    @

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,10 @@
-import AuthProvider from "@/provider/Auth";
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 import { Router } from "next/router";
 import Progress from "@badrap/bar-of-progress";
 import PlausibleProvider from "next-plausible";
+import "@/styles/globals.css";
 
 const progress = new Progress({
   className: "bar-of-progress",
@@ -26,10 +25,8 @@ export default function App({ Component, pageProps }: AppProps) {
           href="https://cdn.uploadfly.cloud/xCnc4L/favicon-orange-mPuTin-huL.png"
         />
       </Head>
-      <AuthProvider>
-        <Toaster />
-        <Component {...pageProps} />
-      </AuthProvider>
+      <Toaster />
+      <Component {...pageProps} />
     </PlausibleProvider>
   );
 }
