5a804ba5f7960b2cfd0dc93cb6920e1d13c18c98
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-20 13:53:08 +0100

    middleware

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verifyAuth } from "./lib/auth";
+
+const middleware = async (req: NextRequest) => {
+  const token = req.cookies.get("refresh_token")?.value;
+
+  let verifiedToken = null;
+  try {
+    verifiedToken = token && (await verifyAuth(token));
+  } catch (error) {}
+
+  if (!verifiedToken) {
+    if (
+      req.nextUrl.pathname !== "/login" &&
+      req.nextUrl.pathname !== "/signup"
+    ) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  if (verifiedToken) {
+    const { username } = verifiedToken;
+
+    if (req.url.includes("/login")) {
+      return NextResponse.redirect(new URL(`/${username}`, req.url));
+    }
+
+    if (req.url.includes("/signup")) {
+      return NextResponse.redirect(new URL(`/${username}`, req.url));
+    }
+  }
+};
+
+export const config = {
+  matcher: [
+    "/((?!api/|_next/|_proxy/|_static|_vercel|favicon.ico|sitemap.xml|robots.txt|logo.svg|grid-bg.png).*)",
+  ],
+};
+
+export default middleware;
