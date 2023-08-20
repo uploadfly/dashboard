4fd2425d3ccece9291b5821b3112f9907bdaa375
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 00:33:39 +0100

    created axios interceptor

--- configs/axios.ts
+++ configs/axios.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+let isRefreshing = false;
+let refreshSubscribers = [];
+
 const axiosAuthInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_AUTH_URL,
   withCredentials: true,
@@ -13,6 +16,36 @@ const axiosInstance = axios.create({
 const axiosUploadflyInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_UPLOADFLY_URL,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response.status === 401) {
+      const originalRequest = error.config;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+        try {
+          await axiosAuthInstance.post("/refresh");
+          return axiosInstance(originalRequest);
+        } catch (refreshError) {
+          return Promise.reject(refreshError);
+        } finally {
+          isRefreshing = false;
+        }
+      } else {
+        return new Promise((resolve) => {
+          refreshSubscribers.push(() => {
+            resolve(axiosInstance(originalRequest));
+          });
+        });
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export {
   axiosAuthInstance as axiosAuth,
