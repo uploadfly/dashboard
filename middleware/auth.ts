4adb641fabfbbba51e8ea5fe6d9d7fdb4af5866d
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-21 00:34:06 +0100

    changed error status code

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -10,7 +10,7 @@ const authenticateToken = async (
 ) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return res.status(400).json({ message: "Token is missing in request" });
+    return res.status(401).json({ message: "Token is missing in request" });
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as {
