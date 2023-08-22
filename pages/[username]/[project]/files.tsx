9023376b1b2c10fb6d4b088366fa26e409ae1ed7
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-22 16:27:25 +0100

    .

--- pages/[username]/[project]/files.tsx
+++ pages/[username]/[project]/files.tsx
@@ -52,52 +52,39 @@ const Files = () => {
       {files.length === 0 ? (
         <NoFiles />
       ) : (
-        <div className="flex gap-5 overflow-auto">
+        <div className="flex gap-5">
           <div
             className={`h-full w-full flex relative transition-all ${
               selectedFile ? "mr-96" : "mr-0"
             }`}
           >
-            <table className="border-collapse table-auto w-full text-sm">
-              <thead className="bg-uf-accent/10">
-                <tr>
-                  {tableHeads.map((head, index) => (
-                    <th
-                      className="border-b border-uf-accent/80 font-medium py-2 text-uf-light text-left pl-5 whitespace-nowrap"
-                      key={index}
-                    >
-                      {head}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="">
-                {files.map((file) => (
-                  <File
-                    key={file.id}
-                    name={file.name}
-                    size={file.size}
-                    type={file.type}
-                    selected={selectedFile == file}
-                    uploaded={file.created_at}
-                    onClick={() => {
-                      // if (selectedFiles.includes(file)) {
-                      //   setSelectedFiles(
-                      //     selectedFiles.filter((f) => f !== file)
-                      //   );
-                      //   return;
-                      // }
-                      // setSelectedFiles([...selectedFiles, file]);
-                      if (selectedFile === file) {
-                        setSelectedFile(null);
-                        return;
-                      }
-                      setSelectedFile(file);
-                    }}
-                  />
-                ))}
-              </tbody>
-            </table>
+            <div className="flex gap-7 justify-start items-center w-fit flex-wrap">
+              {files.map((file) => (
+                <File
+                  key={file.id}
+                  name={file.name}
+                  size={file.size}
+                  type={file.type}
+                  selected={selectedFile == file}
+                  uploaded={file.created_at}
+                  onClick={() => {
+                    // if (selectedFiles.includes(file)) {
+                    //   setSelectedFiles(
+                    //     selectedFiles.filter((f) => f !== file)
+                    //   );
+                    //   return;
+                    // }
+                    // setSelectedFiles([...selectedFiles, file]);
+                    if (selectedFile === file) {
+                      setSelectedFile(null);
+                      return;
+                    }
+                    setSelectedFile(file);
+                  }}
+                />
+              ))}
+            </div>
+
             <div
               className={`h-fit bg-[#1E1E1E] fixed right-5 transition-all rounded-md ${
                 selectedFile ? "w-96" : "w-0"
