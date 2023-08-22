0f63ef359a10c187e0146a6a235577527f49e287
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-22 16:27:00 +0100

    updated files view

--- components/File.tsx
+++ components/File.tsx
@@ -1,6 +1,9 @@
 import fileSize from "file-size";
 import moment from "moment";
 import { BiRightArrow } from "react-icons/bi";
+import { BsImage } from "react-icons/bs";
+import { HiEllipsisVertical, HiOutlineDocumentText } from "react-icons/hi2";
+import { MdAudiotrack, MdOutlineVideoLibrary } from "react-icons/md";
 
 export const truncate = (str: string, length: number = 20) => {
   if (str.length > length) {
@@ -25,31 +28,31 @@ const File = ({
   uploaded: Date;
 }) => {
   return (
-    <tr
-      className={`${
-        selected && "bg-slate-800/60 hover:bg-slate-800/70"
-      } hover:bg-slate-800/30`}
-      onClick={onClick}
-    >
-      <td
-        className="border-b border-slate-700 p-4  text-slate-400"
-        title={name}
-      >
-        {truncate(name)}
-      </td>
-      <td
-        className="border-b border-slate-700 p-4 text-slate-400"
-        title={moment(uploaded).calendar()}
-      >
-        {moment(uploaded).fromNow()}
-      </td>
-      <td className="border-b border-slate-700 p-4 text-slate-400">
-        {truncate(type)}
-      </td>
-      <td className="border-b border-slate-700 p-4 text-slate-400 whitespace-nowrap">
-        {fileSize(size).human("si")}
-      </td>
-    </tr>
+    <div className="w-[260px] py-1 px-2 h-[280px] rounded-md bg-black/20 border-2 border-white/10 hover:border-uf-accent/50 transition-colors cursor-pointer flex flex-col justify-between items-center">
+      <div className="flex justify-between w-full">
+        <small>{truncate(name)}</small>
+        <small>{fileSize(size).human("si")}</small>
+      </div>
+      <div className="text-6xl">
+        {type.includes("image") ? (
+          <BsImage />
+        ) : type.includes("video") ? (
+          <MdOutlineVideoLibrary />
+        ) : type.includes("audio") ? (
+          <MdAudiotrack />
+        ) : (
+          <HiOutlineDocumentText />
+        )}
+      </div>
+      <div className="w-full flex justify-between">
+        <div className="">
+          <small>{truncate(type)}</small>
+        </div>
+        <button>
+          <HiEllipsisVertical />
+        </button>
+      </div>
+    </div>
   );
 };
 
