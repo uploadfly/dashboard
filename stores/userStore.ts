c4dd01664b5ccf7a52e9a4b4ba0d03905ef48fcb
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-24 11:38:40 +0100

    updated user store

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,22 +1,13 @@
-import { axios } from "@/configs/axios";
 import { create } from "zustand";
 
 interface UserStore {
-  user: { username: string; email: string } | null;
-  setUser: () => void;
+  user: { username: string; email?: string } | null;
+  setUser: (userData: { username: string; email?: string }) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
   user: null,
-  setUser: async () => {
-    if (!useUserStore.getState().user) {
-      try {
-        const res = await axios.get("/user");
-        const userData = res.data;
-        set({ user: userData });
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    }
+  setUser: (userData) => {
+    set({ user: userData });
   },
 }));
