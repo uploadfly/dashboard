37b082f2a9a3b15d345c50c4959e474c323372b2
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-24 10:09:03 +0100

    many many things

--- components/AccountSettings/Card.tsx
+++ components/AccountSettings/Card.tsx
@@ -5,11 +5,17 @@ const Card = ({
   children,
   description,
   subtext,
+  disabled,
+  onClick,
+  loading,
 }: {
   title: string;
   description: string;
   children: ReactNode;
   subtext: string;
+  onClick: () => void;
+  disabled: boolean;
+  loading: boolean;
 }) => {
   return (
     <div className="w-full border-2 border-white/10 rounded-md py-5 px-5 flex flex-col items-start gap-6">
@@ -18,8 +24,12 @@ const Card = ({
       {children}
       <div className="flex items-center justify-between w-full flex-wrap gap-3">
         <p className="text-sm">{subtext}</p>
-        <button className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold w-full lg:w-fit">
-          Update
+        <button
+          className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold w-full lg:w-fit disabled:opacity-60 disabled:cursor-not-allowed hover:bg-uf-accent transition-colors"
+          onClick={onClick}
+          disabled={disabled}
+        >
+          {loading ? "Loading..." : "Save"}
         </button>
       </div>
     </div>
