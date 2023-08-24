9a057aa0b4bfbe524ffe297721f571819c15abde
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-24 05:02:13 +0100

    Card/>

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,3 +1,4 @@
+import Card from "@/components/AccountSettings/Card";
 import AccountSettingsLayout from "@/layouts/AccountSettingsLayout";
 import { useUserStore } from "@/stores/userStore";
 
@@ -8,12 +9,12 @@ const Settings = () => {
       <div>
         <h1 className="text-2xl font-semibold">Account Settings</h1>
         <div className="mt-5 flex gap-10 flex-col">
-          <div className="w-full border-2 border-white/10 rounded-md py-5 px-5 flex flex-col items-start gap-6">
-            <h2 className="text-xl font-semibold">Username</h2>
-            <p className="text-sm">
-              Your URL namespace within your Uploadfly dashboard.
-            </p>
-            <div className="flex items-center rounded-md border border-uf-accent/50 focus-within:border-uf-accent transition-colors w-[400px]">
+          <Card
+            title="Username"
+            description=" Your URL namespace within your Uploadfly dashboard."
+            subtext="Should be between 3 and 50 characters long."
+          >
+            <div className="flex items-center rounded-md border border-uf-accent/50 focus-within:border-uf-accent transition-colors lg:w-[400px] w-full">
               <p className="bg-uf-accent/20 p-2 rounded-md">
                 beta.uploadfly.cloud/
               </p>
@@ -23,49 +24,28 @@ const Settings = () => {
                 defaultValue={user?.username}
               />
             </div>
-            <div className="flex items-center justify-between w-full">
-              <p className="text-sm">
-                Should be between 3 and 50 characters long.
-              </p>
-              <button className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold">
-                Update
-              </button>
-            </div>
-          </div>
-          <div className="w-full border-2 border-white/10 rounded-md py-5 px-5 flex flex-col items-start gap-6">
-            <h2 className="text-xl font-semibold">Name</h2>
-            <p className="text-sm">Please enter your first or nick name.</p>
+          </Card>
+          <Card
+            title="Name"
+            description="Please enter your first or nick name."
+            subtext="Should be between 2 and 32 characters long."
+          >
             <input
               type="text"
-              className="bg-transparent border border-uf-accent/30 focus:border-uf-accent/70 transition-colors outline-none rounded-md pl-3 py-2 w-[400px]"
+              className="bg-transparent border border-uf-accent/30 focus:border-uf-accent/70 transition-colors outline-none rounded-md pl-3 py-2 lg:w-[400px] w-full"
             />
-            <div className="flex items-center justify-between w-full">
-              <p className="text-sm">
-                Should be between 2 and 32 characters long.
-              </p>
-              <button className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold">
-                Update
-              </button>
-            </div>
-          </div>
-          <div className="w-full border-2 border-white/10 rounded-md py-5 px-5 flex flex-col items-start gap-6">
-            <h2 className="text-xl font-semibold">Email</h2>
-            <p className="text-sm">
-              Enter the email address you want to use access your Uploadfly
-              account.
-            </p>
+          </Card>
+          <Card
+            title="Email"
+            description="Enter the email address you want to use access your Uploadfly."
+            subtext="We will require you to verify this."
+          >
             <input
               type="text"
-              className="bg-transparent border border-uf-accent/30 focus:border-uf-accent/70 transition-colors outline-none rounded-md pl-3 py-2 w-[400px]"
+              className="bg-transparent border border-uf-accent/30 focus:border-uf-accent/70 transition-colors outline-none rounded-md pl-3 py-2 lg:w-[400px] w-full"
               defaultValue={user?.email}
             />
-            <div className="flex items-center justify-between w-full">
-              <p className="text-sm">We will require you to verify this.</p>
-              <p className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold">
-                Save
-              </p>
-            </div>
-          </div>
+          </Card>
         </div>
       </div>
     </AccountSettingsLayout>
