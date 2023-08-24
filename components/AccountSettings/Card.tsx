ac2c2862bfd19f8cecfeb46417dccf47e50ecef3
akinloluwami <akinkunmiisrael41@gmail.com>
2023-08-24 05:05:04 +0100

    updated button style for mobile

--- components/AccountSettings/Card.tsx
+++ components/AccountSettings/Card.tsx
@@ -16,9 +16,9 @@ const Card = ({
       <h2 className="text-xl font-semibold">{title}</h2>
       <p className="text-sm">{description}</p>
       {children}
-      <div className="flex items-center justify-between w-full">
+      <div className="flex items-center justify-between w-full flex-wrap gap-3">
         <p className="text-sm">{subtext}</p>
-        <button className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold">
+        <button className="bg-uf-accent/80 text-white px-5 py-2 rounded-md font-semibold w-full lg:w-fit">
           Update
         </button>
       </div>
