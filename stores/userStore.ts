31346eb9bb153f9856877e955dc57a860a27eef2
akinloluwami <akinkunmioye42@gmail.com>
2023-08-29 23:19:02 +0100

    accept user name

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,8 +1,12 @@
 import { create } from "zustand";
 
 interface UserStore {
-  user: { username: string; email?: string } | null;
-  setUser: (userData: { username: string; email?: string }) => void;
+  user: { username: string; email?: string; name?: string } | null;
+  setUser: (userData: {
+    username: string;
+    email?: string;
+    name?: string;
+  }) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
