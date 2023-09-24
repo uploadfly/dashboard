2fdea0db16b4e8d2a55e6f785ff465ec0477e238
David Onifade <davidicfola@gmail.com>
2023-09-24 23:02:11 +0100

    Modify: showPassword input field to icon

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,16 @@
-import { toastErrorConfig, toastSuccessConfig } from "@/configs/toast";
-import AuthLayout from "@/layouts/AuthLayout";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { useRouter } from "next/router";
-import { axiosAuth } from "@/configs/axios";
-import { RiLoader5Fill } from "react-icons/ri";
+import { toastErrorConfig, toastSuccessConfig } from '@/configs/toast';
+import AuthLayout from '@/layouts/AuthLayout';
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/router';
+import { axiosAuth } from '@/configs/axios';
+import { RiLoader5Fill } from 'react-icons/ri';
+import { RiEyeLine } from 'react-icons/ri';
+import { RiEyeOffLine } from 'react-icons/ri';
 
 const Login = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
   const [showPassword, setShowPassword] = useState<boolean>(false);
@@ -18,25 +20,25 @@ const Login = () => {
     const returnTo = query.returnTo ? query.returnTo.toString() : null;
 
     if (!email) {
-      toast("Email is required", toastErrorConfig);
+      toast('Email is required', toastErrorConfig);
       return;
     }
 
     if (!password) {
-      toast("Password is required", toastErrorConfig);
+      toast('Password is required', toastErrorConfig);
       return;
     }
 
     setLoading(true);
 
     try {
-      const response = await axiosAuth.post("/login", {
+      const response = await axiosAuth.post('/login', {
         email,
         password,
       });
       const userData = response?.data?.user;
       if (userData && userData.username) {
-        toast("Login successful", toastSuccessConfig);
+        toast('Login successful', toastSuccessConfig);
         if (returnTo) {
           router.push(returnTo);
           return;
@@ -45,7 +47,7 @@ const Login = () => {
         return;
       }
     } catch (error: any) {
-      const errorMessage = error.response?.data?.message || "Login failed";
+      const errorMessage = error.response?.data?.message || 'Login failed';
       toast(errorMessage, toastErrorConfig);
     } finally {
       setLoading(false);
@@ -56,19 +58,17 @@ const Login = () => {
     <AuthLayout
       text="Welcome back"
       question={{
-        route: "/signup",
-        text: "Signup",
-        title: "New to Uploadfly?",
+        route: '/signup',
+        text: 'Signup',
+        title: 'New to Uploadfly?',
       }}
-      pageName="Login"
-    >
+      pageName="Login">
       <form
         className="flex flex-col z-40"
         onSubmit={(e) => {
           e.preventDefault();
           loginWithEmail();
-        }}
-      >
+        }}>
         {/**I had a long travel today, just typing this comment so I don't lose my GitHub streak*/}
         <input
           type="email"
@@ -78,26 +78,25 @@ const Login = () => {
           onChange={(e) => setEmail(e.target.value.toLowerCase().trim())}
           name="email"
         />
-        <input
-          type={showPassword ? "text" : "password"}
-          className="input"
-          placeholder="Shh...it's a secret"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          name="password"
-        />
-        <div className="flex items-center gap-2 mt-2">
+        <div className="relative">
+          <button
+            type="button"
+            className="absolute right-0 mt-6 mr-3"
+            onClick={() => setShowPassword(!showPassword)}>
+            {!showPassword ? <RiEyeOffLine /> : <RiEyeLine />}
+          </button>
           <input
-            type="checkbox"
-            name="Show password"
-            onChange={() => setShowPassword(!showPassword)}
+            type={showPassword ? 'text' : 'password'}
+            className="input"
+            placeholder="Shh...it's a secret"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            name="password"
           />
-          <p>Show password</p>
         </div>
         <button
           className="bg-uf-accent w-full rounded-md py-2 h-10 flex items-center justify-center text-[#1e1e1e] font-bold hover:scale-105 transition-all disabled:cursor-not-allowed mt-4"
-          disabled={loading}
-        >
+          disabled={loading}>
           {loading ? (
             <RiLoader5Fill className="animate-spin text-2xl" />
           ) : (
