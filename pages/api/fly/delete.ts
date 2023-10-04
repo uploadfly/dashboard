91cf20936ffb59a638d94f29d2d8e50308ada780
akinloluwami <akinkunmioye42@gmail.com>
2023-10-04 11:00:34 +0100

    try catch

--- pages/api/fly/delete.ts
+++ pages/api/fly/delete.ts
@@ -6,82 +6,79 @@ import { NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: ExtendedRequest, res: NextApiResponse) => {
-  const fly_id = req.query.fly_id as string;
+  try {
+    const fly_id = req.query.fly_id as string;
 
-  if (!fly_id) {
-    res.status(400).json({ message: "Fly ID is missing in request" });
-    return;
-  }
-  const fly = await prisma.fly.findUnique({
-    where: {
-      id: fly_id,
-    },
-  });
-
-  if (!fly) {
-    res.status(400).json({ message: "Invalid fly ID" });
-    return;
-  }
-
-  if (fly.user_id !== req.user.id) {
-    res
-      .status(403)
-      .json({ message: "You are not authorized to delete this fly" });
-    return;
-  }
-
-  const userApiKey = await prisma.apikey.findFirst({
-    where: {
-      user_id: req.user.id,
-      active: true,
-      permission: "full",
-    },
-    select: {
-      key: true,
-    },
-  });
-
-  if (!userApiKey)
-    return res.status(401).json({
-      message: "Create an API key with full access to carry out this action",
+    if (!fly_id) {
+      res.status(400).json({ message: "Fly ID is missing in request" });
+      return;
+    }
+    const fly = await prisma.fly.findUnique({
+      where: {
+        id: fly_id,
+      },
     });
 
-  await axios
-    .delete(
-      `${process.env.NEXT_PUBLIC_UPLOADFLY_URL}/delete/all?fly_id=${fly_id}`,
-      {
-        headers: {
-          Authorization: `Bearer ${userApiKey?.key}`,
-        },
-      }
-    )
-    .then(async () => {
-      await prisma.fly.delete({
-        where: {
-          id: fly_id,
-        },
-      });
+    if (!fly) {
+      res.status(400).json({ message: "Invalid fly ID" });
+      return;
+    }
 
-      res.status(200).json({
-        message: "Fly deleted successfully",
-      });
-    })
-    .catch((error) => {
-      res.status(500).json({ message: "Something went wrong." });
+    if (fly.user_id !== req.user.id) {
+      res
+        .status(403)
+        .json({ message: "You are not authorized to delete this fly" });
+      return;
+    }
+
+    const userApiKey = await prisma.apikey.findFirst({
+      where: {
+        user_id: req.user.id,
+        active: true,
+        permission: "full",
+      },
+      select: {
+        key: true,
+      },
     });
 
-  await prisma.apikey
-    .deleteMany({
+    if (!userApiKey)
+      return res.status(401).json({
+        message: "Create an API key with full access to carry out this action",
+      });
+
+    await axios
+      .delete(
+        `${process.env.NEXT_PUBLIC_UPLOADFLY_URL}/delete/all?fly_id=${fly_id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${userApiKey?.key}`,
+          },
+        }
+      )
+      .then(async () => {
+        await prisma.fly.delete({
+          where: {
+            id: fly_id,
+          },
+        });
+
+        res.status(200).json({
+          message: "Fly deleted successfully",
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ message: "Something went wrong." });
+      });
+
+    await prisma.apikey.deleteMany({
       where: {
         user_id: req.user.id,
       },
-    })
-    .catch((err: any) => {
-      res.status(500).json({
-        message: "Something went wrong",
-      });
-      console.log(err.response);
     });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
 };
 
 export default allowMethods(["DELETE"])(
