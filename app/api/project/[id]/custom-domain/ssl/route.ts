39ceff7b16e058ad6f3ebda93d7f668e8d128b9e
akinloluwami <akinkunmioye42@gmail.com>
2023-10-16 03:17:00 +0100

    removed console log

--- app/api/project/[id]/custom-domain/ssl/route.ts
+++ app/api/project/[id]/custom-domain/ssl/route.ts
@@ -102,10 +102,6 @@ export async function GET(request: Request) {
       },
     };
 
-    console.log(
-      `${data.DNS.name?.split(".")[0]}.${data.DNS.name?.split(".")[1]}`
-    );
-
     return NextResponse.json({ ...data }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error" }, { status: 500 });
