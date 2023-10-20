1c270a5d10f47bbd89c1f5f8225f98088a5f9537
akinloluwami <akinkunmioye42@gmail.com>
2023-10-20 09:10:23 +0100

    feat: send request to request a new certificate.

--- pages/[username]/[project]/settings/domains.tsx
+++ pages/[username]/[project]/settings/domains.tsx
@@ -1,7 +1,32 @@
+import { axios } from "@/configs/axios";
+import { toastErrorConfig, toastSuccessConfig } from "@/configs/toast";
 import SettingsLayout from "@/layouts/SettingsLayout";
+import { useFlyStore } from "@/stores/flyStore";
+import { useState } from "react";
+import toast from "react-hot-toast";
 import { BiPlus } from "react-icons/bi";
 
 const Domains = () => {
+  const { fly } = useFlyStore();
+  const [domainName, setDomainName] = useState("");
+
+  const requestCertificate = async () => {
+    try {
+      const { data } = await axios.post(
+        `/project/${fly.name}/custom-domain/certificate`,
+        { domainName }
+      );
+      toast.success(data.message, toastSuccessConfig);
+    } catch (error: any) {
+      toast(
+        error.response.data.message || "Something went wrong.",
+        toastErrorConfig
+      );
+    }
+  };
+
+  const [showForm, setShowForm] = useState(false);
+
   return (
     <SettingsLayout>
       <h1 className="font-semibold text-2xl">Add a custom domain name</h1>
@@ -12,10 +37,31 @@ const Domains = () => {
         </span>{" "}
         serve your files at your own domain.
       </p>
-      <button className="flex items-center bg-uf-accent text-uf-light rounded-md px-5 py-2 font-semibold mt-4">
-        <BiPlus />
-        Add domain
-      </button>
+      {showForm ? (
+        <div className="flex items-center gap-5 mt-5">
+          <input
+            type="text"
+            className="py-2 pl-2 rounded-md w-[300px]"
+            onChange={(e) => setDomainName(e.target.value.toLowerCase())}
+            value={domainName}
+          />
+          <button
+            className="flex items-center bg-uf-accent text-uf-light rounded-md px-5 py-2 font-semibold"
+            onClick={requestCertificate}
+          >
+            <BiPlus />
+            Add domain
+          </button>
+        </div>
+      ) : (
+        <button
+          className="flex items-center bg-uf-accent text-uf-light rounded-md px-5 py-2 font-semibold mt-4"
+          onClick={() => setShowForm(!showForm)}
+        >
+          <BiPlus />
+          Add domain
+        </button>
+      )}
     </SettingsLayout>
   );
 };
