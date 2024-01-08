2b32dbb9fefc552d90c16bacaf071b0d58481031
akinloluwami <akinkunmioye42@gmail.com>
2024-01-08 10:27:40 +0100

    pass custom data for webhook

--- pages/[username]/[project]/settings/billing-plans.tsx
+++ pages/[username]/[project]/settings/billing-plans.tsx
@@ -1,9 +1,26 @@
 import SettingsLayout from "@/layouts/SettingsLayout";
+import { useFlyStore } from "@/stores/flyStore";
+import { useUserStore } from "@/stores/userStore";
+import Head from "next/head";
 
 const BillingAndPlans = () => {
+  const { user } = useUserStore();
+  const { fly } = useFlyStore();
+
+  const openCheckout = () => {
+    const checkoutUrl = `https://uploadfly.lemonsqueezy.com/checkout/buy/1c8a1972-9b91-49b8-a30e-c147eadc27f0?checkout[email]=${user?.email}&checkout[custom][user_id]=${user?.id}&checkout[custom][project_id]=${fly?.id}`;
+
+    window.location.href = checkoutUrl;
+  };
+
   return (
     <SettingsLayout>
+      <Head>
+        <script src="https://app.lemonsqueezy.com/js/lemon.js" defer></script>
+      </Head>
       <h1>Biiling and Plans</h1>
+
+      <button onClick={openCheckout}>Upgrade to pro</button>
     </SettingsLayout>
   );
 };
