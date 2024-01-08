418daee44603aa315f310383e6bd6b7635f9c9ca
akinloluwami <akinkunmioye42@gmail.com>
2024-01-08 21:52:31 +0100

    ...

--- app/api/project/[id]/route.ts
+++ app/api/project/[id]/route.ts
@@ -1,4 +1,5 @@
 import prisma from "@/prisma";
+import { generateApiKey } from "@/utils/generateApiKey";
 import { getProject } from "@/utils/getProject";
 import { getUserId } from "@/utils/getUserId";
 import axios from "axios";
@@ -8,75 +9,85 @@ export async function DELETE(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  try {
-    const projectId = params.id;
-    const userId = await getUserId();
+  const projectId = params.id;
+  const userId = await getUserId();
 
-    if (!userId) {
-      return NextResponse.json(
-        { message: "Unauthorized request." },
-        { status: 401 }
-      );
-    }
-
-    if (!projectId) {
-      return NextResponse.json(
-        { message: "Project ID is required." },
-        { status: 400 }
-      );
-    }
-
-    const project = await getProject(projectId);
-
-    if (!project) {
-      return NextResponse.json(
-        { message: "Project not found." },
-        { status: 404 }
-      );
-    }
-
-    if (project.user_id !== userId) {
-      return NextResponse.json(
-        { message: "You are not authorized to delete this project." },
-        { status: 403 }
-      );
-    }
-
-    const projectApiKey = await prisma.apikey.findFirst({
-      where: { fly_id: projectId, active: true, permission: "full" },
-    });
-
-    if (!projectApiKey) {
-      return NextResponse.json(
-        { message: "Create a full-access API key to carry out this action." },
-        { status: 400 }
-      );
-    }
-
-    await axios.delete(
-      `${process.env.NEXT_PUBLIC_UPLOADFLY_URL}/delete/all?fly_id=${projectId}`,
-      {
-        headers: {
-          Authorization: `Bearer ${projectApiKey.key}`,
-        },
-      }
-    );
-
-    await prisma.$transaction([
-      prisma.apikey.delete({ where: { id: projectApiKey.id } }),
-      prisma.log.deleteMany({ where: { fly_id: projectId } }),
-      prisma.file.deleteMany({ where: { fly_id: projectId } }),
-      prisma.fly.delete({ where: { id: projectId } }),
-    ]);
-
+  if (!userId) {
     return NextResponse.json(
-      { message: "Project deleted successfully." },
-      { status: 200 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Project not deleted." },
-      { status: 500 }
+      { message: "Unauthorized request." },
+      { status: 401 }
     );
   }
+
+  if (!projectId) {
+    return NextResponse.json(
+      { message: "Project ID is required." },
+      { status: 400 }
+    );
+  }
+
+  const project = await getProject(projectId);
+
+  if (!project) {
+    return NextResponse.json(
+      { message: "Project not found." },
+      { status: 404 }
+    );
+  }
+
+  if (project.user_id !== userId) {
+    return NextResponse.json(
+      { message: "You are not authorized to delete this project." },
+      { status: 403 }
+    );
+  }
+
+  let projectApiKey = await prisma.apikey.findFirst({
+    where: {
+      fly_id: projectId,
+      active: true,
+      permission: "full",
+    },
+  });
+
+  if (!projectApiKey) {
+    await prisma.apikey.create({
+      data: {
+        fly_id: projectId,
+        permission: "full",
+        user_id: userId,
+        key: generateApiKey(),
+        name: "key",
+      },
+    });
+
+    projectApiKey = await prisma.apikey.findFirst({
+      where: {
+        fly_id: projectId,
+        active: true,
+        permission: "full",
+      },
+    });
+  }
+
+  await axios.delete(
+    `${process.env.NEXT_PUBLIC_UPLOADFLY_URL}/delete/all?fly_id=${projectId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${projectApiKey?.key}`,
+      },
+    }
+  );
+
+  await prisma.$transaction([
+    prisma.apikey.delete({ where: { id: projectApiKey?.id } }),
+    prisma.log.deleteMany({ where: { fly_id: projectId } }),
+    prisma.file.deleteMany({ where: { fly_id: projectId } }),
+    prisma.fly.delete({ where: { id: projectId } }),
+  ]);
+
+  return NextResponse.json(
+    { message: "Project deleted successfully." },
+    { status: 200 }
+  );
 }
