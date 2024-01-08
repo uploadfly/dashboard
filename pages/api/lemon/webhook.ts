43b68969a7baf503cb3f9f225441338a58df081b
akinloluwami <akinkunmioye42@gmail.com>
2024-01-08 12:20:19 +0100

    payment webhook

--- pages/api/lemon/webhook.ts
+++ pages/api/lemon/webhook.ts
@@ -1,3 +1,4 @@
+import prisma from "@/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -12,8 +13,43 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const userEmail = req.body.data.user_email;
   const customerId = req.body.data.customer_id;
+  const userId = req.body.meta.custom_data.user_id;
+  const projectId = req.body.meta.custom_data.project_id;
 
-  if (event === "subscription_created") {
+  console.log(req.body);
+
+  try {
+    if (event === "subscription_created") {
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          lemon_customer_id: customerId,
+        },
+      });
+
+      const project = await prisma.fly.findUnique({
+        where: {
+          id: projectId,
+        },
+      });
+
+      await prisma.fly.update({
+        where: {
+          id: project?.id,
+        },
+        data: {
+          plan: "pro",
+          storage: 100000000000,
+        },
+      });
+
+      res.status(200).json({ message: "Subscription created" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+    console.log(error);
   }
 };
 
