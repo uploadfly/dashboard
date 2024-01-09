de461429322e505f9982268e028043ee1283771c
akinloluwami <akinkunmioye42@gmail.com>
2024-01-09 02:59:09 +0100

    update style

--- pages/[username]/[project]/settings/billing-plans.tsx
+++ pages/[username]/[project]/settings/billing-plans.tsx
@@ -30,7 +30,7 @@ const BillingAndPlans = () => {
           button={
             <button
               onClick={fly.plan === "free" ? openCheckout : openCustomerPotal}
-              className="flex gap-2 bg-uf-accent text-uf-light mt-10 rounded-md py-2 w-[380px] items-center justify-center font-semibold hover:scale-105 transition-all disabled:opacity-50 disabled:cursor-not-allowed"
+              className="flex gap-2 bg-uf-accent text-uf-light mt-10 rounded-md py-2 max-w-sm w-full items-center justify-center font-semibold hover:scale-105 transition-all disabled:opacity-50 disabled:cursor-not-allowed"
             >
               {fly.plan === "free" ? "Upgrade to Pro" : "Manage Plan"}
             </button>
