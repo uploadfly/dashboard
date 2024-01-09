9df3605e38fa29fb0245c9edc63cfae0be7d4135
akinloluwami <akinkunmioye42@gmail.com>
2024-01-09 11:41:26 +0100

    rewrote lemon webhook in app directory

---
+++ app/api/callback/lemon/route.ts
@@ -0,0 +1,125 @@
+import prisma from "@/prisma";
+import { deleteAllFiles } from "@/utils/deleteAllFiles";
+import dayjs from "dayjs";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const event = body.meta.event_name as
+    | "subscription_created"
+    | "subscription_paused"
+    | "subscription_unpaused"
+    | "subscription_resumed"
+    | "subscription_cancelled"
+    | "subscription_expired";
+
+  const signature = request.headers.get("x-signature");
+
+  if (!signature) {
+    return NextResponse.json(
+      { message: "No signature found" },
+      { status: 401 }
+    );
+  }
+  const secret = process.env.LEMON_SECRET;
+
+  // const data = await request.text()
+
+  const customerId = body.data.customer_id;
+  const userId = body.meta.custom_data.user_id;
+  const projectId = body.meta.custom_data.project_id;
+
+  try {
+    const project = await prisma.fly.findUnique({
+      where: {
+        id: projectId,
+      },
+    });
+
+    if (event === "subscription_created") {
+      await prisma.user.update({
+        where: {
+          id: userId,
+        },
+        data: {
+          lemon_customer_id: customerId,
+        },
+      });
+
+      const project = await prisma.fly.findUnique({
+        where: {
+          id: projectId,
+        },
+      });
+
+      await prisma.fly.update({
+        where: {
+          id: project?.id,
+        },
+        data: {
+          plan: "pro",
+          storage: 100000000000,
+        },
+      });
+
+      return NextResponse.json(
+        { message: "Subscription created" },
+        {
+          status: 200,
+        }
+      );
+    }
+
+    if (event === "subscription_paused") {
+      await prisma.fly.update({
+        where: {
+          id: projectId,
+        },
+        data: {
+          paused: true,
+          paused_at: dayjs().toDate(),
+        },
+      });
+      return NextResponse.json(
+        { message: "Subscription has been paused" },
+        {
+          status: 200,
+        }
+      );
+    }
+
+    if (event === "subscription_unpaused") {
+      await prisma.fly.update({
+        where: {
+          id: projectId,
+        },
+        data: {
+          paused: false,
+          paused_at: null,
+        },
+      });
+      return NextResponse.json(
+        { message: "Subscription has been resumed" },
+        {
+          status: 200,
+        }
+      );
+    }
+
+    if (event === "subscription_expired") {
+      await deleteAllFiles({
+        projectId,
+        userId,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Internal server error", error },
+      {
+        status: 500,
+      }
+    );
+  }
+}
