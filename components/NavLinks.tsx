5d2b19ec022c836b0762f8e1c653d65d479b0148
akinloluwami <akinkunmioye42@gmail.com>
2024-05-18 09:13:31 +0100

    link to plans page

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -64,8 +64,10 @@ const NavLinks = ({ loading }: { loading: boolean }) => {
 
   const { user } = useUserStore();
 
+  const billingRoute = router.pathname.split("/")[4];
+
   return (
-    <div className="lg:px-10 px-5 w-full overflow-x-auto">
+    <div className="lg:px-10 px-5 w-full overflow-x-auto flex items-center justify-between">
       <div className={loading ? "pointer-events-none" : "flex gap-12 mt-3"}>
         {links.map((link, i) => {
           const href = `/${user?.username}/${flyName}${link.path}`;
@@ -90,6 +92,15 @@ const NavLinks = ({ loading }: { loading: boolean }) => {
           );
         })}
       </div>
+
+      {!router.asPath.includes("plans") && (
+        <Link
+          href={`/${user?.username}/${flyName}/settings/billing-plans`}
+          className="bg-uf-accent rounded-md py-2 px-10 flex items-center justify-center text-white font-semibold transition-all"
+        >
+          Choose a plan
+        </Link>
+      )}
     </div>
   );
 };
