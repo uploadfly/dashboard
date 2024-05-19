f88523b79f794cc62663c72aef308e6d48b5b05c
akinloluwami <akinkunmioye42@gmail.com>
2024-05-19 16:52:34 +0100

    update middleware logic

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,36 +1,33 @@
-import { NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import jwt from "jsonwebtoken";
 import prisma from "@/prisma";
-import { ExtendedRequest, UserModel } from "@/interfaces";
+import { UserModel } from "@/interfaces";
 
-const authenticateToken = async (
-  req: ExtendedRequest,
-  res: NextApiResponse,
-  next: () => void
-) => {
-  const token = req.cookies.access_token;
-  if (!token) {
-    return res.status(401).json({ message: "Token is missing in request" });
-  }
+const withAuth =
+  async (handler: any) => async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies.access_token;
+    if (!token) {
+      return res.status(401).json({ message: "Token is missing in request" });
+    }
 
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as {
-    id: string;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY as string) as {
+      id: string;
+    };
+
+    const user_id = decoded.id;
+
+    if (!user_id)
+      return res.status(400).json({ message: "Missing user id in payload" });
+
+    const user = (await prisma.user.findUnique({
+      where: {
+        id: user_id,
+      },
+    })) as UserModel;
+
+    req.user = user;
+
+    return await handler(req, res);
   };
 
-  const user_id = decoded.id;
-
-  if (!user_id)
-    return res.status(400).json({ message: "Missing user id in payload" });
-
-  const user = (await prisma.user.findUnique({
-    where: {
-      id: user_id,
-    },
-  })) as UserModel;
-
-  req.user = user;
-
-  next();
-};
-
-export default authenticateToken;
+export default withAuth;
