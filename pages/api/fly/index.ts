2ca534c3c54dc10e094b85ffdf0e036deff1519c
akinloluwami <akinkunmioye42@gmail.com>
2024-05-19 17:48:29 +0100

    remove try catch

--- pages/api/fly/index.ts
+++ pages/api/fly/index.ts
@@ -6,32 +6,28 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { allowMethods } from "next-method-guard";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const flies = await prisma.fly.findMany({
-      where: {
-        user_id: req.user.id,
-      },
-      select: {
-        name: true,
-        id: true,
-        used_storage: true,
-        updated_at: true,
-        storage: true,
-        plan: true,
-        paused: true,
-      },
-    });
+  const flies = await prisma.fly.findMany({
+    where: {
+      user_id: req.user.id,
+    },
+    select: {
+      name: true,
+      id: true,
+      used_storage: true,
+      updated_at: true,
+      storage: true,
+      plan: true,
+      paused: true,
+    },
+  });
 
-    const fliesWithUsedStorageAsNumber = flies.map((fly: any) => ({
-      ...fly,
-      used_storage: Number(fly.used_storage),
-      storage: Number(fly.storage),
-    }));
+  const fliesWithUsedStorageAsNumber = flies.map((fly: any) => ({
+    ...fly,
+    used_storage: Number(fly.used_storage),
+    storage: Number(fly.storage),
+  }));
 
-    res.json(fliesWithUsedStorageAsNumber);
-  } catch (error) {
-    res.status(500).json({ error });
-  }
+  return res.json(fliesWithUsedStorageAsNumber);
 };
 
 export default withErrorHandling(withAuth(handler), ["GET"]);
